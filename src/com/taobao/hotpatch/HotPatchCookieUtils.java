695acaafa9030433b6cf094784cb77443ba813ba
lianzhong.wlz@taobao.com <lianzhong.wlz@taobao.com>
2014-05-26 19:16:46 +0800

    update code

--- src/com/taobao/hotpatch/HotPatchCookieUtils.java
+++ src/com/taobao/hotpatch/HotPatchCookieUtils.java
@@ -4,8 +4,6 @@ import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Properties;
 
-import android.content.Intent;
-import android.text.TextUtils;
 import android.util.Log;
 
 import com.taobao.android.dexposed.XC_MethodReplacement;
@@ -20,6 +18,7 @@ public class HotPatchCookieUtils implements IPatch {
 
 	@Override
 	public void handlePatch(PatchParam arg0) throws Throwable {
+		/*
 		Class<?> cls = null;
 		try {
 			cls = arg0.classLoader
@@ -59,7 +58,7 @@ public class HotPatchCookieUtils implements IPatch {
 					}
 
 				});
-		
+		*/
 		Class<?> clsSessionManager = null;
 		try {
 			clsSessionManager = arg0.classLoader
@@ -75,7 +74,7 @@ public class HotPatchCookieUtils implements IPatch {
 					@Override
 					protected Object replaceHookedMethod(MethodHookParam arg0)
 							throws Throwable {
-						Log.d("HotPatch_pkg", "start hotpatch SessionManager injectCookie");		
+						Log.d("HotPatch_pkg", "start hotpatch SessionManager clearSessionInfo");		
 						try {
 							Object main = (Object) arg0.thisObject;
 							Method setSid = main.getClass().getMethod("setSid",String.class);
@@ -92,10 +91,10 @@ public class HotPatchCookieUtils implements IPatch {
 							
 							Method setUserName = main.getClass().getMethod("setUserName",String.class);
 							setUserName.invoke(main, new Object[]{null});
-//							try {
-//								Method injectCookie = main.getClass().getDeclaredMethod("injectCookie",String[].class);
-//								injectCookie.invoke(main, new Object[]{null});
-//					        } catch (Exception e) {
+							try {
+								Method injectCookie = main.getClass().getDeclaredMethod("injectCookie",String[].class);
+								injectCookie.invoke(main, new Object[]{null});
+					        } catch (Exception e) {
 //					        	e.printStackTrace();
 //					        	Method removeUTCookie = main.getClass().getDeclaredMethod("removeUTCookie",Intent.class);
 //					        	removeUTCookie.setAccessible(true);
@@ -105,21 +104,21 @@ public class HotPatchCookieUtils implements IPatch {
 //								removeWeitaoCookie.setAccessible(true);
 //								removeWeitaoCookie.invoke(main, new Object[]{null});
 
-//								List<Cookie>  mCookie = (List<Cookie>)XposedHelpers.getObjectField(main, "mCookie");
-//					            mCookie.clear();
-//								Method removeStorage = main.getClass().getDeclaredMethod("removeStorage",String.class);
-//								removeStorage.setAccessible(true);
-//								removeStorage.invoke(main, new Object[]{null});
-//					        }
-							Log.d("HotPatch_pkg", "hotpatch SessionManager injectCookie not set null");
-					        Log.d("HotPatch_pkg", "hotpatch SessionManager injectCookie success");
+								List<Cookie>  mCookie = (List<Cookie>)XposedHelpers.getObjectField(main, "mCookie");
+					            mCookie.clear();
+								Method removeStorage = main.getClass().getDeclaredMethod("removeStorage",String.class);
+								removeStorage.setAccessible(true);
+								removeStorage.invoke(main, new Object[]{"injectCookie"});
+					        }
+							Log.d("HotPatch_pkg", "hotpatch SessionManager clearSessionInfo enable catch exception");
+					        Log.d("HotPatch_pkg", "hotpatch SessionManager clearSessionInfo success");
 	                        Properties bundle = new Properties();
-	                        bundle.put("desc", "patch success on SessionManager injectCookie");
+	                        bundle.put("desc", "patch success on SessionManager clearSessionInfo");
 	                        TBS.Ext.commitEvent("hotpatch_pkg",bundle);
 						} catch (Exception e) {
-							Log.d("HotPatch_pkg", "hotpatch SessionManager injectCookie failed");
+							Log.d("HotPatch_pkg", "hotpatch SessionManager clearSessionInfo failed");
 		                    Properties bundle = new Properties();
-		                    bundle.put("desc", "patch failed on SessionManager injectCookie");
+		                    bundle.put("desc", "patch failed on SessionManager clearSessionInfo");
 		                    TBS.Ext.commitEvent("hotpatch_pkg",bundle);
 							e.printStackTrace();
 						}
