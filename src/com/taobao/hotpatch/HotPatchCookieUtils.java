52126c4c6c4a2e6b35376e6d2ed408287274510c
lianzhong.wlz@taobao.com <lianzhong.wlz@taobao.com>
2014-05-26 18:04:41 +0800

    update code

--- src/com/taobao/hotpatch/HotPatchCookieUtils.java
+++ src/com/taobao/hotpatch/HotPatchCookieUtils.java
@@ -1,14 +1,18 @@
 package com.taobao.hotpatch;
 
 import java.lang.reflect.Method;
+import java.util.List;
 import java.util.Properties;
 
+import android.content.Intent;
 import android.text.TextUtils;
 import android.util.Log;
 
 import com.taobao.android.dexposed.XC_MethodReplacement;
 import com.taobao.android.dexposed.XposedBridge;
+import com.taobao.android.dexposed.XposedHelpers;
 import com.taobao.login4android.refactor.session.Cookie;
+import com.taobao.statistic.TBS;
 import com.taobao.updatecenter.hotpatch.IPatch;
 import com.taobao.updatecenter.hotpatch.PatchCallback.PatchParam;
 
@@ -25,7 +29,6 @@ public class HotPatchCookieUtils implements IPatch {
 			Log.e("HotPatch_pkg", "invoke CookieUtils class failed", e);
 			e.printStackTrace();
 		}
-		
 		XposedBridge.findAndHookMethod(cls, "getHttpDomin", Cookie.class,
 				new XC_MethodReplacement() {
 
@@ -36,6 +39,7 @@ public class HotPatchCookieUtils implements IPatch {
 						try {
 							Cookie cookie = (Cookie) arg0.args[0];
 						   	String host =  cookie.domain;
+						   	Log.d("HotPatch_pkg", "start hotpatch CookieUtils getHttpDomin cookie.domain = " + cookie.domain);	
 					    	if(!TextUtils.isEmpty(host) && host.startsWith(".")){
 								 host = host.substring(1);
 							}
@@ -43,12 +47,12 @@ public class HotPatchCookieUtils implements IPatch {
 					        Log.d("HotPatch_pkg", "hotpatch CookieUtils getHttpDomin success");
 	                        Properties bundle = new Properties();
 	                        bundle.put("desc", "patch success on CookieUtils getHttpDomin");
-//	                        TBS.Ext.commitEvent("hotpatch_pkg",bundle);
+	                        TBS.Ext.commitEvent("hotpatch_pkg",bundle);
 						} catch (Exception e) {
 							Log.d("HotPatch_pkg", "hotpatch CookieUtils getHttpDomin failed");
 		                    Properties bundle = new Properties();
 		                    bundle.put("desc", "patch failed on CookieUtils getHttpDomin");
-//		                    TBS.Ext.commitEvent("hotpatch_pkg",bundle);
+		                    TBS.Ext.commitEvent("hotpatch_pkg",bundle);
 							e.printStackTrace();
 						}
 						return null;
@@ -77,21 +81,22 @@ public class HotPatchCookieUtils implements IPatch {
 							Method setSid = main.getClass().getMethod("setSid",String.class);
 							setSid.invoke(main, new Object[]{null});
 							
-							Method setEcode = main.getClass().getDeclaredMethod("setEcode",String.class);
+							Method setEcode = main.getClass().getMethod("setEcode",String.class);
 							setEcode.invoke(main, new Object[]{null});
 							
-							Method setNick = main.getClass().getDeclaredMethod("setNick",String.class);
+							Method setNick = main.getClass().getMethod("setNick",String.class);
 							setNick.invoke(main, new Object[]{null});
 							
-							Method setUserId = main.getClass().getDeclaredMethod("setUserId",String.class);
+							Method setUserId = main.getClass().getMethod("setUserId",String.class);
 							setUserId.invoke(main, new Object[]{null});
 							
-							Method setUserName = main.getClass().getDeclaredMethod("setUserName",String.class);
+							Method setUserName = main.getClass().getMethod("setUserName",String.class);
 							setUserName.invoke(main, new Object[]{null});
-							try {
-								Method injectCookie = main.getClass().getDeclaredMethod("injectCookie",String[].class);
-								injectCookie.invoke(main, new Object[]{null});
-					        } catch (Exception e) {
+//							try {
+//								Method injectCookie = main.getClass().getDeclaredMethod("injectCookie",String[].class);
+//								injectCookie.invoke(main, new Object[]{null});
+//					        } catch (Exception e) {
+//					        	e.printStackTrace();
 //					        	Method removeUTCookie = main.getClass().getDeclaredMethod("removeUTCookie",Intent.class);
 //					        	removeUTCookie.setAccessible(true);
 //					        	removeUTCookie.invoke(main, new Object[]{null});
@@ -102,21 +107,20 @@ public class HotPatchCookieUtils implements IPatch {
 
 //								List<Cookie>  mCookie = (List<Cookie>)XposedHelpers.getObjectField(main, "mCookie");
 //					            mCookie.clear();
-//
-//					            // 清除持久化cookie
 //								Method removeStorage = main.getClass().getDeclaredMethod("removeStorage",String.class);
 //								removeStorage.setAccessible(true);
 //								removeStorage.invoke(main, new Object[]{null});
-					        }
+//					        }
+							Log.d("HotPatch_pkg", "hotpatch SessionManager injectCookie not set null");
 					        Log.d("HotPatch_pkg", "hotpatch SessionManager injectCookie success");
 	                        Properties bundle = new Properties();
 	                        bundle.put("desc", "patch success on SessionManager injectCookie");
-//	                        TBS.Ext.commitEvent("hotpatch_pkg",bundle);
+	                        TBS.Ext.commitEvent("hotpatch_pkg",bundle);
 						} catch (Exception e) {
 							Log.d("HotPatch_pkg", "hotpatch SessionManager injectCookie failed");
 		                    Properties bundle = new Properties();
 		                    bundle.put("desc", "patch failed on SessionManager injectCookie");
-//		                    TBS.Ext.commitEvent("hotpatch_pkg",bundle);
+		                    TBS.Ext.commitEvent("hotpatch_pkg",bundle);
 							e.printStackTrace();
 						}
 						return null;
