cd21168ffbffbfa3d1980680b52dcc9ac6bfccf6
lianzhong.wlz@taobao.com <lianzhong.wlz@taobao.com>
2014-06-09 19:45:08 +0800

    update log

--- src/com/taobao/hotpatch/HotPatchFeedStreamViewControler.java
+++ src/com/taobao/hotpatch/HotPatchFeedStreamViewControler.java
@@ -2,7 +2,7 @@ package com.taobao.hotpatch;
 
 import java.util.Properties;
 
-import android.taobao.util.TaoLog;
+import android.util.Log;
 
 import com.taobao.android.dexposed.XC_MethodReplacement;
 import com.taobao.android.dexposed.XposedBridge;
@@ -19,9 +19,9 @@ public class HotPatchFeedStreamViewControler implements IPatch {
 		try {
 			FeedStreamViewControler  = arg0.classLoader
 					.loadClass("com.taobao.tao.allspark.feed.viewcontroller.FeedStreamViewControler");
-			TaoLog.Logd("HotPatch_pkg", "invoke FeedStreamViewControler  class success");
+			Log.d("HotPatch_pkg", "invoke FeedStreamViewControler  class success");
 		} catch (ClassNotFoundException e) {
-			TaoLog.Loge("HotPatch_pkg", "invoke FeedStreamViewControler  class failed" + e.toString());
+			Log.e("HotPatch_pkg", "invoke FeedStreamViewControler  class failed" + e.toString());
 		}
 		
 		XposedBridge.findAndHookMethod(FeedStreamViewControler, "jumpToDarenSquare", new XC_MethodReplacement() {
@@ -29,7 +29,7 @@ public class HotPatchFeedStreamViewControler implements IPatch {
 					@Override
 					protected Object replaceHookedMethod(MethodHookParam args0)
 							throws Throwable {
-						TaoLog.Logd("HotPatch_pkg", "start hotpatch FeedStreamViewControler jumpToDarenSquare");
+						Log.d("HotPatch_pkg", "start hotpatch FeedStreamViewControler jumpToDarenSquare");
 
 						// replace start
 						Nav.from(arg0.context).toUri("http://h5.m.taobao.com/we/plaza2.html");
@@ -37,7 +37,7 @@ public class HotPatchFeedStreamViewControler implements IPatch {
 						Properties bundle = new Properties();
 						bundle.put("desc",	"patch success on FeedStreamViewControler jumpToDarenSquare");
 						TBS.Ext.commitEvent("hotpatch_pkg", bundle);
-						TaoLog.Logd("HotPatch_pkg", "end hotpatch FeedStreamViewControler jumpToDarenSquare");
+						Log.d("HotPatch_pkg", "end hotpatch FeedStreamViewControler jumpToDarenSquare");
 						return null;
 					}
 
