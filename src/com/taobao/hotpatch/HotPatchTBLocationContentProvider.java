2cb97aa49c12f230ff5712f80a129e5b9c8a0186
lianzhong.wlz@taobao.com <lianzhong.wlz@taobao.com>
2014-06-09 16:26:55 +0800

    update code

--- src/com/taobao/hotpatch/HotPatchTBLocationContentProvider.java
+++ src/com/taobao/hotpatch/HotPatchTBLocationContentProvider.java
@@ -7,7 +7,6 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
-import android.taobao.util.TaoLog;
 import android.text.TextUtils;
 import android.util.Log;
 
