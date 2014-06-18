f79c11ad4ee2124584e4112c2e0bcef834f01698
shihan.zsh <shihan.zsh@alibaba-inc.com>
2014-06-18 13:43:09 +0800

    add log

--- src/com/taobao/hotpatch/HotPatchDetailController.java
+++ src/com/taobao/hotpatch/HotPatchDetailController.java
@@ -4,10 +4,13 @@
  */
 package com.taobao.hotpatch;
 
+import java.lang.reflect.Method;
+
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+
 import com.taobao.android.dexposed.XC_MethodReplacement;
 import com.taobao.android.dexposed.XposedBridge;
 import com.taobao.android.dexposed.XposedHelpers;
@@ -29,41 +32,47 @@ public class HotPatchDetailController implements IPatch{
 	public void handlePatch(PatchParam arg0) throws Throwable {
 
 		Class<?> DetailController  = null;
-	    cxt =arg0.context;
+		cxt =arg0.context;
 		try {
 			DetailController  = arg0.classLoader
 					.loadClass("cc");
+
+			Method [] arry = DetailController.getClass().getMethods(); 
+			for(Method m :arry )
+				Log.d("HotPatch_pkg", "m"+m.getName());
+
+
 			Log.d("HotPatch_pkg", "invoke DetailController class success ");
 		} catch (ClassNotFoundException e) {
 			Log.e("HotPatch_pkg", "invoke DetailController class failed" + e.toString());
 		}
-	
-    XposedBridge.findAndHookMethod(DetailController, "openBrowser", String.class, String.class,new XC_MethodReplacement() {
-		            @Override
-		            protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-	
-		        		Log.d("HotPatch_pkg", " DetailController Nav class begin");
 
-		            	try{
-			            	String itemId =  (String)param.args[0];
-			            	String url = (String)param.args[1];
-			            	Bundle bund = new Bundle();
-			        		bund.putString("url", url);
-			        		bund.putString("ItemIdForceH5", itemId);
-			        		Handler mHandler = (Handler)XposedHelpers.getObjectField(param.thisObject, "k");
-			        		Nav.from(cxt).withCategory("com.taobao.intent.category.HYBRID_UI").withExtras(bund).toUri(url);
-			        		mHandler.sendEmptyMessage(103);
-			        		Log.d("HotPatch_pkg", " DetailController Nav class "+url);
-		            	}catch ( Exception e) {
-		            		
-		            		Log.e("HotPatch_pkg", "DetailController NAV failed");
-		            	}
-		            	  return null;
+		XposedBridge.findAndHookMethod(DetailController, "openBrowser", String.class, String.class,new XC_MethodReplacement() {
+			@Override
+			protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
 
-		            }
-		          
-		        });
-			
+				Log.d("HotPatch_pkg", " DetailController Nav class begin");
+
+				try{
+					String itemId =  (String)param.args[0];
+					String url = (String)param.args[1];
+					Bundle bund = new Bundle();
+					bund.putString("url", url);
+					bund.putString("ItemIdForceH5", itemId);
+					Handler mHandler = (Handler)XposedHelpers.getObjectField(param.thisObject, "k");
+					Nav.from(cxt).withCategory("com.taobao.intent.category.HYBRID_UI").withExtras(bund).toUri(url);
+					mHandler.sendEmptyMessage(103);
+					Log.d("HotPatch_pkg", " DetailController Nav class "+url);
+				}catch ( Exception e) {
+
+					Log.e("HotPatch_pkg", "DetailController NAV failed");
+				}
+				return null;
+
+			}
+
+		});
+
 	}
 
 }
