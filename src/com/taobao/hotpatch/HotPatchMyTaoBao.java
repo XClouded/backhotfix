cb0739b7a3c0039568fd37b8c0c390deaecd069b
chunping.hcp <chunping.hcp@alibaba-inc.com>
2014-07-17 19:59:14 +0800

    add log

--- src/com/taobao/hotpatch/HotPatchMyTaoBao.java
+++ src/com/taobao/hotpatch/HotPatchMyTaoBao.java
@@ -5,7 +5,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.taobao.atlas.framework.Atlas;
 import android.taobao.atlas.framework.BundleImpl;
-import android.taobao.util.TaoLog;
 import android.util.Log;
 
 import com.taobao.android.dexposed.XC_MethodHook;
@@ -22,61 +21,67 @@ public class HotPatchMyTaoBao implements IPatch {
 	@Override
 	public void handlePatch(final PatchParam arg0) throws Throwable {
 		// TODO Auto-generated method stub
-		Log.d("HotPatch_pkg", "start HotPatchMyTaoBao handlePatch");
+		Log.e("HotPatch_pkg", "start HotPatchMyTaoBao handlePatch");
 		try {
 			mytaobao = (BundleImpl) Atlas.getInstance().getBundle(
 					"com.taobao.mytaobao");
 			if (mytaobao == null) {
-				Log.d("HotPatch_pkg", "mytaobao bundle is null");
+				Log.e("HotPatch_pkg", "mytaobao bundle is null");
 				return;
 			}
 			mytaobaoActivity = mytaobao.getClassLoader().loadClass(
 					"com.taobao.tao.mytaobao.MyTaoBaoActivity");
-			Log.d("HotPatch_pkg", "mytaobaoActivity loadClass  success");
+			Log.e("HotPatch_pkg", "mytaobaoActivity loadClass  success");
 
 		} catch (ClassNotFoundException e) {
-			Log.d("HotPatch_pkg",
+			Log.e("HotPatch_pkg",
 					"invoke mytaobaoActivity class failed" + e.toString());
 			return;
 		}
 		try {
+			Log.e("HotPatch_pkg","HotPatchMyTaoBao 1");
 		XposedBridge.findAndHookMethod(mytaobaoActivity, "onCreate",
 				Bundle.class, new XC_MethodHook() {
 
 					@Override
 					protected void afterHookedMethod(MethodHookParam param)
 							throws Throwable {
-
-						Log.d("HotPatch_pkg",
+						Log.e("HotPatch_pkg","HotPatchMyTaoBao 2");
+						Log.e("HotPatch_pkg",
 								"afterHookedMethod onCreate start");
 						Intent intent = (Intent) XposedHelpers.callMethod(
 								param.thisObject, "getIntent");
+						Log.e("HotPatch_pkg","HotPatchMyTaoBao 3");
 						Uri uri = intent.getData();
 						// String struri =
 						// "http://h5.m.taobao.com/awp/mtb/mtb.htm#!/awp/mtb/olist.htm";
 						String struri = null;
+						Log.e("HotPatch_pkg","HotPatchMyTaoBao 4");
 						if (uri != null)
 							struri = uri.toString();
+						Log.e("HotPatch_pkg","HotPatchMyTaoBao 5 struri="+struri);
 
 						if (struri != null) {
-							TaoLog.Logi("MyTaoBaoActivity", struri);
+							Log.e("HotPatch_pkg","HotPatchMyTaoBao 6");
 							if (struri.contains("/awp/mtb/olist.htm")) {
-								Log.d("HotPatch_pkg",
+								Log.e("HotPatch_pkg",
 										"afterHookedMethod nav to NAV_URL_LOGISTIC_DETAIL");
 								Intent intent1 = new Intent();
+								Log.e("HotPatch_pkg","HotPatchMyTaoBao 7");
 								intent1.setAction("android.intent.action.VIEW");
+								Log.e("HotPatch_pkg","HotPatchMyTaoBao 8");
 								intent1.setData(Uri.parse("http://tb.cn/x/wl"));
 								intent1.setClassName(arg0.context,
 										"com.taobao.tao.logistic.LogisticListActivity");
-								Log.d("HotPatch_pkg",
+								Log.e("HotPatch_pkg",
 										"afterHookedMethod start LogisticListActivity");
 								XposedHelpers.callMethod(param.thisObject,
 										"startActivity", intent1);
-								Log.d("HotPatch_pkg",
+								Log.e("HotPatch_pkg",
 										"afterHookedMethod finish mytaobao");
 								XposedHelpers.callMethod(param.thisObject,
 										"finish");
-								Log.d("HotPatch_pkg",
+								Log.e("HotPatch_pkg",
 										"afterHookedMethod finish");
 								// startActivity(intent1);
 								// finish();
@@ -85,10 +90,10 @@ public class HotPatchMyTaoBao implements IPatch {
 					}
 				});
 		} catch(Exception e) {
-			Log.d("HotPatch_pkg",
+			Log.e("HotPatch_pkg",
 					"invoke mytaobaoActivity class failed2: " + e.toString());
 		}
-		Log.d("HotPatch_pkg", "HotPatchSessionManager hotpatch finish");
+		Log.e("HotPatch_pkg", "HotPatchSessionManager hotpatch finish");
 	}
 
 }
