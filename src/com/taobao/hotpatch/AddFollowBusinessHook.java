059d7e1b7ab26d0606638b8e27b2299f68ffb93e
zhiwei.pzw <zhiwei.pzw@alibaba-inc.com>
2014-07-30 12:48:00 +0800

    测试AddFollowBusiness

--- src/com/taobao/hotpatch/AddFollowBusinessHook.java
+++ src/com/taobao/hotpatch/AddFollowBusinessHook.java
@@ -5,6 +5,7 @@ import android.taobao.atlas.framework.Atlas;
 import android.taobao.atlas.framework.BundleImpl;
 import android.util.Log;
 import android.taobao.apirequest.ApiID;
+import android.taobao.windvane.util.TaoLog;
 
 import com.taobao.we.mtop.adapter.ApiResult;
 import com.taobao.android.dexposed.XC_MethodReplacement;
@@ -35,58 +36,71 @@ public class AddFollowBusinessHook implements IPatch {
             Log.d("HotPatch_pkg", "invoke AddFollowBusiness class failed" + e.toString());
             return;
         }
-		XposedBridge.findAndHookMethod(clazz, "execute", String.class,Long.class,String.class,    //修复execute方法
-				new XC_MethodReplacement() {
-			
-			@Override
-			protected Object replaceHookedMethod(MethodHookParam methodParam) throws Throwable {
-				String MTOP_FOLLOW_ADD="mtop.cybertron.follow.add";
-				String sid=(String) methodParam.args[0];
-				Long   pubAccountId=(Long) methodParam.args[1];
-				String origin=(String) methodParam.args[2];
-				BasicParam param=new BasicParam();
-				BasicSingleRequest request=new BasicSingleRequest(param);
-				request.setAPI_NAME(MTOP_FOLLOW_ADD);
-				request.setNEED_ECODE(true);
-				request.setSid(sid);
-				request.setVERSION("1.0");
-				if(pubAccountId!=null)
-					param.putExtParam("pubAccountId", pubAccountId.longValue());
-				param.putExtParam("origin", origin);
-				BasicSingleBusiness business=new BasicSingleBusiness(Globals.getApplication(), param);
-				final Object listener=XposedHelpers.getObjectField(methodParam.thisObject, "mListener");
-				business.setRemoteBusinessRequestListener(new IRemoteBusinessRequestListener() {
-					
-					@Override
-					public void onSuccess(
-							com.taobao.we.mtop.adapter.BaseRemoteBusiness business,
-							Object context, int requestType, Object data) {
-						if(listener!=null) {
-							try {
-								XposedHelpers.callMethod(listener, "onSuccess");
-							} catch (Exception e) {
-								e.printStackTrace();
-							}
-						}	
-					}
-					
-					@Override
-					public void onError(com.taobao.we.mtop.adapter.BaseRemoteBusiness business,
-							Object context, int requestType, ApiID apiId, ApiResult apiResult) {
-						if(listener!=null) {
-							try {
-								XposedHelpers.callMethod(listener, "onError", apiResult.errDescription);
-							} catch (Exception e) {
-								e.printStackTrace();
-							}
+		try {
+			XposedBridge.findAndHookMethod(clazz, "execute", String.class,Long.class,String.class,    //修复execute方法
+					new XC_MethodReplacement() {
+				
+				@Override
+				protected Object replaceHookedMethod(MethodHookParam methodParam) throws Throwable {
+					TaoLog.d("hotpatch", "begin call");
+					String MTOP_FOLLOW_ADD="mtop.cybertron.follow.add";
+					String sid=(String) methodParam.args[0];
+					Long   pubAccountId=(Long) methodParam.args[1];
+					String origin=(String) methodParam.args[2];
+					BasicParam param=new BasicParam();
+					BasicSingleRequest request=new BasicSingleRequest(param);
+					request.setAPI_NAME(MTOP_FOLLOW_ADD);
+					request.setNEED_ECODE(true);
+					request.setSid(sid);
+					request.setVERSION("1.0");
+					if(pubAccountId!=null)
+						param.putExtParam("pubAccountId", pubAccountId.longValue());
+					param.putExtParam("origin", origin);
+					BasicSingleBusiness business=new BasicSingleBusiness(Globals.getApplication(), param);
+					final Object listener=XposedHelpers.getObjectField(methodParam.thisObject, "mListener");
+					business.setRemoteBusinessRequestListener(new IRemoteBusinessRequestListener() {
+						
+						@Override
+						public void onSuccess(
+								com.taobao.we.mtop.adapter.BaseRemoteBusiness business,
+								Object context, int requestType, Object data) {
+							if(listener!=null) {
+								try {
+									XposedHelpers.callMethod(listener, "onSuccess");
+								} catch (Exception e) {
+									e.printStackTrace();
+								}
+							}	
 						}
 						
-					}
-				});
-				business.sendRequest(request, null, BaseOutDo.class, param.getExtParams());
-				return null;
-			}
-		});
+						@Override
+						public void onError(com.taobao.we.mtop.adapter.BaseRemoteBusiness business,
+								Object context, int requestType, ApiID apiId, ApiResult apiResult) {
+							if(listener!=null) {
+								try {
+									XposedHelpers.callMethod(listener, "onError", apiResult.errDescription);
+								} catch (Exception e) {
+									e.printStackTrace();
+								}
+							}
+							
+						}
+					});
+					business.sendRequest(request, null, BaseOutDo.class, param.getExtParams());
+					return null;
+				}
+			});
+		}catch (Exception e) {
+            TaoLog.d("HotPatch_pkg", "invoke ChatImageManager class failed" + e.toString());
+            e.printStackTrace();
+            return;
+        } catch (Error e) {
+            TaoLog.d("HotPatch_pkg", "invoke ChatImageManager class failed2" + e.toString());
+            e.printStackTrace();
+            return;
+        }
+		
+		TaoLog.d("hotpach", "hook succeed");
 			
 	}
 
