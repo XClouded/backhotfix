7b4c93f54d363867a67f61a29ad99e456b9b2d8d
{韩先琦}({067332}) <xianqi.hanxq@alibaba-inc.com>
2014-08-30 13:29:26 +0800

    加入日志

--- src/com/taobao/hotpatch/PassiveLocationPatch.java
+++ src/com/taobao/hotpatch/PassiveLocationPatch.java
@@ -49,19 +49,27 @@ public class PassiveLocationPatch implements IPatch
 			{
 				try
 				{
+					Log.d("HotPatch_pkg", "checkIfStartSamplingWorking replaceHookedMethod callback invoke start");
 					boolean flag = (Boolean) XposedHelpers.callMethod(param.thisObject, "canSampling");
 					if (flag)
 					{
 						Object object = XposedHelpers.getObjectField(param.thisObject, "mLocationRequester");
+						Log.d("HotPatch_pkg", "get mLocationRequester： " + object);
 						XposedHelpers.callMethod(object, "startLocationSampling", mContext);
+						Log.d("HotPatch_pkg", "callMethod startLocationSampling");
 						XposedHelpers.callMethod(object, "startRegularReportLocationTask", mContext, true);
+						Log.d("HotPatch_pkg", "callMethod startRegularReportLocationTask");
 					}
 					else
 					{
 						mLastLocationFinder = mPassiveLocation.getClassLoader().loadClass("com.taobao.passivelocation.util.LastLocationFinder");
+						Log.d("HotPatch_pkg", "get LastLocationFinder class: " + mLastLocationFinder);
 						Object obj = XposedHelpers.newInstance(mLastLocationFinder, mContext);
+						Log.d("HotPatch_pkg", "newInstance LastLocationFinder: " + obj);
 						XposedHelpers.callMethod(obj, "requestSingleUpdate");
+						Log.d("HotPatch_pkg", "callMethod requestSingleUpdate");
 					}
+					Log.d("HotPatch_pkg", "checkIfStartSamplingWorking replaceHookedMethod callback invoke end");
 				}
 				catch (Exception ex)
 				{
@@ -79,12 +87,15 @@ public class PassiveLocationPatch implements IPatch
 			{
 				try
 				{
+					Log.d("HotPatch_pkg", "asyncUpdateConfig replaceHookedMethod callback invoke start");
 					XposedHelpers.callMethod(param.thisObject, "updateConfig");
+					Log.d("HotPatch_pkg", "asyncUpdateConfig replaceHookedMethod callback invoke end");
 				}
 				catch (Exception ex)
 				{
 					Log.d("HotPatch_pkg", "asyncUpdateConfig replaceHookedMethod callback failed: " + ex.toString());
 				}
+				
 				return null;
 			}
 		});
