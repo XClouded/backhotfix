6998e39b13d110e93930460bf354cc588af993a1
xufu.lg <xufu.lg@taobao.com>
2014-09-25 17:49:41 +0800

    代码回退，把之前引入的两个没用的import 回退

--- src/com/taobao/hotpatch/HotPatchCpEnvManager.java
+++ src/com/taobao/hotpatch/HotPatchCpEnvManager.java
@@ -14,20 +14,18 @@ import com.taobao.hotpatch.patch.IPatch;
 import com.taobao.hotpatch.patch.PatchCallback.PatchParam;
 import com.taobao.securityjni.StaticDataStore;
 import com.taobao.securityjni.tools.DataContext;
-import com.taobao.tao.Globals;
-import com.taobao.tao.TaoApplication;
 
 public class HotPatchCpEnvManager implements IPatch {
 
     private final static String TAG = "HotPatchCpEnvManager";
-    
+
     @Override
     public void handlePatch(PatchParam arg0) throws Throwable {
-        
+
         Log.d(TAG, "HotPatchCpEnvManager start detecting ... ");
-        
+
         Class<?> AutoFocusManager = null;
-        
+
         try {
             BundleImpl cpBundle = (BundleImpl) Atlas.getInstance().getBundle(
                     "com.taobao.caipiao.plugin");
@@ -38,51 +36,51 @@ public class HotPatchCpEnvManager implements IPatch {
             Log.d(TAG, "invoke HotPatchCpEnvManager class failed" + e.toString());
             return;
         }
-        
+
         Log.d(TAG, "loadClass HotPatchCpEnvManager Env success.");
-        
-        XposedBridge.findAndHookMethod(AutoFocusManager, "getAppKey", Context.class, 
+
+        XposedBridge.findAndHookMethod(AutoFocusManager, "getAppKey", Context.class,
                 new XC_MethodReplacement() {
-            
-                @Override
-                protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-                    
-                    Log.d(TAG, "2Begin replaceHookedMethod Env");
-                    
-                    String sAppkey = (String) XposedHelpers.getStaticObjectField(param.method.getDeclaringClass(), "sAppkey");      //public static String sAppkey;
 
-                    Log.d(TAG, "sak===");
-                    
-                    int sVersionStyle =  (Integer) XposedHelpers.getStaticObjectField(param.method.getDeclaringClass(), "sVersionStyle");   // public static int sVersionStyle
-                    
-                    Log.d(TAG, "vs===");
-                    
-                    if (!TextUtils.isEmpty(sAppkey))
-                    {
+                    @Override
+                    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+
+                        Log.d(TAG, "2Begin replaceHookedMethod Env");
+
+                        String sAppkey = (String) XposedHelpers.getStaticObjectField(param.method.getDeclaringClass(), "sAppkey");      //public static String sAppkey;
+
+                        Log.d(TAG, "sak===");
+
+                        int sVersionStyle =  (Integer) XposedHelpers.getStaticObjectField(param.method.getDeclaringClass(), "sVersionStyle");   // public static int sVersionStyle
+
+                        Log.d(TAG, "vs===");
+
+                        if (!TextUtils.isEmpty(sAppkey))
+                        {
+                            return sAppkey;
+                        }
+
+                        Log.d(TAG, "sAak != null");
+
+                        if (sVersionStyle <= 0)
+                        {
+                            XposedHelpers.callStaticMethod(param.method.getDeclaringClass(), "getVersionStyle", new Class<?>[] { Context.class }, param.args[0]);     //调用当前类static方法getVersionStyle,Env.getVersionStyle(ctx);
+                        }
+
+                        Log.d(TAG, "Begin DataContext");
+
+                        DataContext dc = new DataContext();
+                        dc.index = 0;
+                        StaticDataStore sds = new StaticDataStore((ContextWrapper) param.args[0]);
+                        sAppkey = sds.getAppKey(dc);
+
+                        Log.d(TAG, "end replaceHookedMethod Env ");
+
                         return sAppkey;
-                    }
-                    
-                    Log.d(TAG, "sAak != null");
 
-                    if (sVersionStyle <= 0)
-                    {
-                        XposedHelpers.callStaticMethod(param.method.getDeclaringClass(), "getVersionStyle", new Class<?>[] { Context.class }, param.args[0]);     //调用当前类static方法getVersionStyle,Env.getVersionStyle(ctx);
                     }
 
-                    Log.d(TAG, "Begin DataContext");
-                    
-                    DataContext dc = new DataContext();
-                    dc.index = 0;
-                    StaticDataStore sds = new StaticDataStore((ContextWrapper) param.args[0]);
-                    sAppkey = sds.getAppKey(dc);
-                    
-                    Log.d(TAG, "end replaceHookedMethod Env ");
-                    
-                    return sAppkey;
-                    
-                }
-                
-        });
+                });
     }
 
 }
