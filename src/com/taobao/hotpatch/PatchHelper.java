9f0b9b6b9c7cc5fb894d12194f4776d082fed4b5
lianzhong.wlz@taobao.com <lianzhong.wlz@taobao.com>
2014-10-11 14:59:02 +0800

    add helper and template

---
+++ src/com/taobao/hotpatch/PatchHelper.java
@@ -0,0 +1,49 @@
+package com.taobao.hotpatch;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.taobao.atlas.framework.Atlas;
+import android.taobao.atlas.framework.BundleImpl;
+
+public class PatchHelper {
+
+	public static boolean isRunInMainProcess(Context context) {
+		if ("com.taobao.taobao".equals(getProcessName(context))) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	// 获得当前的进程名字
+    public static String getProcessName(Context context) {
+        int pid = android.os.Process.myPid();
+        ActivityManager mActivityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningAppProcessInfo appProcess : mActivityManager.getRunningAppProcesses()) {
+            if (appProcess.pid == pid) {
+                return appProcess.processName;
+            }
+        }
+        return "";
+    }
+    
+    public static Class<?> loadClass(Context context,String className, String bundleName) {
+    	if (bundleName == null) {
+    		try {
+    			return context.getClassLoader().loadClass(className);
+    		} catch (ClassNotFoundException e) {
+    			return null;
+    		}
+    	} else {
+    		BundleImpl bundle = (BundleImpl) Atlas.getInstance().getBundle(bundleName);
+    		if (bundle == null) {
+    			return null;
+    		}
+    		try {
+    			return bundle.getClassLoader().loadClass(className);
+    		} catch (ClassNotFoundException e) {
+    			return null;
+    		}
+    	} 	
+    }
+}
