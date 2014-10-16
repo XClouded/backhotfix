91721e93aca03bdb495bfac390e6757a37964bc7
lianzhong.wlz@taobao.com <lianzhong.wlz@taobao.com>
2014-10-16 15:43:45 +0800

    修复被动定位的问题

---
+++ src/com/taobao/hotpatch/LocationAlarmPatch.java
@@ -0,0 +1,57 @@
+package com.taobao.hotpatch;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+import com.taobao.android.dexposed.XC_MethodHook;
+import com.taobao.android.dexposed.XposedBridge;
+import com.taobao.hotpatch.patch.IPatch;
+import com.taobao.hotpatch.patch.PatchCallback;
+
+public class LocationAlarmPatch implements IPatch {
+
+	private final static String ACTION_UPDATE_CONFIG = "com.taobao.passivelocation.Update_Config";
+
+	@Override
+	public void handlePatch(PatchCallback.PatchParam arg0) throws Throwable {
+		final Context context = arg0.context;
+//
+//		if (!PatchHelper.isRunInMainProcess(context)) {
+//			return;
+//		}
+
+		AlarmManager alarms = (AlarmManager) context
+				.getSystemService(Context.ALARM_SERVICE);
+		Intent intent = new Intent(ACTION_UPDATE_CONFIG);
+		PendingIntent pendingIntent = PendingIntent.getService(context, 0,
+				intent, PendingIntent.FLAG_UPDATE_CURRENT);
+		alarms.cancel(pendingIntent);
+
+		final Class<?> LocationParameterConfiger = PatchHelper.loadClass(context,
+				"com.taobao.passivelocation.util.LocationParameterConfiger",
+				"com.taobao.passivelocation");
+		if (LocationParameterConfiger == null) {
+			return;
+		}
+
+		// TODO 入参跟上面描述相同，只是最后参数为XC_MethodHook。
+		// beforeHookedMethod和afterHookedMethod，可以根据需要只实现其一
+		XposedBridge.findAndHookMethod(LocationParameterConfiger,
+				"LocationParameterConfiger", Context.class,
+				new XC_MethodHook() {
+					@Override
+					protected void afterHookedMethod(MethodHookParam param)
+							throws Throwable {
+						AlarmManager alarms = (AlarmManager) context
+								.getSystemService(Context.ALARM_SERVICE);
+						Intent intent = new Intent(ACTION_UPDATE_CONFIG);
+						PendingIntent pendingIntent = PendingIntent.getService(context, 0,
+								intent, PendingIntent.FLAG_UPDATE_CURRENT);
+						alarms.cancel(pendingIntent);
+					}
+				});
+
+	}
+}
