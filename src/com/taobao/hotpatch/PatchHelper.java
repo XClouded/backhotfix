37fe9a2c1f0edfedf6e9bd34e6b8c8c044e250b9
煜晨 <yuxi.wyx@alibaba-inc.com>
2014-10-21 17:42:28 +0800

    add log

--- src/com/taobao/hotpatch/PatchHelper.java
+++ src/com/taobao/hotpatch/PatchHelper.java
@@ -4,6 +4,7 @@ import android.app.ActivityManager;
 import android.content.Context;
 import android.taobao.atlas.framework.Atlas;
 import android.taobao.atlas.framework.BundleImpl;
+import android.util.Log;
 
 public class PatchHelper {
 
@@ -30,18 +31,23 @@ public class PatchHelper {
     public static Class<?> loadClass(Context context,String className, String bundleName) {
     	if (bundleName == null) {
     		try {
+    			Log.d("hotpatch", "loadClass：bundleName == null & return class");
     			return context.getClassLoader().loadClass(className);
     		} catch (ClassNotFoundException e) {
+    			Log.d("hotpatch", "loadClass：bundleName == null & ClassNotFoundException");
     			return null;
     		}
     	} else {
     		BundleImpl bundle = (BundleImpl) Atlas.getInstance().getBundle(bundleName);
     		if (bundle == null) {
+    			Log.d("hotpatch", "loadClass：bundleName != null & bundle == null");
     			return null;
     		}
     		try {
+    			Log.d("hotpatch", "loadClass：bundleName != null & return class");
     			return bundle.getClassLoader().loadClass(className);
     		} catch (ClassNotFoundException e) {
+    			Log.d("hotpatch", "loadClass：bundleName != null & ClassNotFoundException");
     			return null;
     		}
     	} 	
