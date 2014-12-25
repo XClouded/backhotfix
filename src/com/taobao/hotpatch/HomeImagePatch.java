5a8cf761968c8b0c9be1d48e7934bee3869fe95f
jianbai <jianbai.gbj@alibaba-inc.com>
2014-12-25 10:57:43 +0800

    通过系统反射方法调用

--- src/com/taobao/hotpatch/HomeImagePatch.java
+++ src/com/taobao/hotpatch/HomeImagePatch.java
@@ -23,29 +23,26 @@ public class HomeImagePatch implements IPatch {
 	            return;
 	     }
 		 final  Class<?>  SmoothScrollFeatureClass =  PatchHelper.loadClass(context, "com.taobao.uikit.extend.feature.features.SmoothScrollFeature", null);
-         
-		 XposedBridge.findAndHookMethod(MainActivity3, "hiddenWelcome", new XC_MethodHook() {
+         final  Class<?>  AbsFeature = PatchHelper.loadClass(context, "com.taobao.uikit.feature.features.AbsFeature", null);
+		 if(SmoothScrollFeatureClass == null 
+				 || AbsFeature == null){
+			 Log.e("MainActivity3", "class feature not found, return。");
+	            return;
+		 }
+         XposedBridge.findAndHookMethod(MainActivity3, "hiddenWelcome", new XC_MethodHook() {
 	            @Override
 	            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
 	                Log.e("MainActivity3", "beforeHookedMethod enter");
 	                try {
 		                	 Object TListView =  XposedHelpers.getObjectField(param.thisObject, "mPutiListView");
-		                	 
-		                
-		                 Log.e("MainActivity3", "add feature success" + TListView + "super class" + 	 TListView.getClass().getSuperclass() + " class " + SmoothScrollFeatureClass);
-		                 Method[] methods = TListView.getClass().getMethods();
-		                 if(methods != null){
-		                	    for(Method method : methods){
-		                	       	Log.e("MainActivity3", "TListView method " + method.getName()  + " " + method.getReturnType());
-		                	    }
-		                 }
-		                     
-		                 
-		                 Object object = XposedHelpers.callMethod(TListView, "findFeature",new Class[] {SmoothScrollFeatureClass}, SmoothScrollFeatureClass);
+                         Log.e("MainActivity3", "add feature success" + TListView + "super class" + 	 TListView.getClass().getSuperclass() + " class " + SmoothScrollFeatureClass);
+                         Method findFeature = TListView.getClass().getMethod("findFeature", AbsFeature);
+                         Object  object = findFeature.invoke(TListView, SmoothScrollFeatureClass);
 		                	 Log.e("MainActivity3", "find feature success" + object);
 		                	 if(object == null){
 		                		 Object SmoothScrollFeature = SmoothScrollFeatureClass.newInstance();
-		                		 XposedHelpers.callMethod(TListView, "addFeature",new Class[] {SmoothScrollFeatureClass}, SmoothScrollFeature);
+		                		 Method addFeature = TListView.getClass().getMethod("addFeature", AbsFeature);
+		                		 addFeature.invoke(TListView, SmoothScrollFeature);
 		                		 Log.e("MainActivity3", "add feature success");
 		                	 }
 	                } catch (Throwable e) {
