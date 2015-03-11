ef13de9b95362f9f4a8984f0a820883525812fde
lianzhong.wlz@taobao.com <lianzhong.wlz@taobao.com>
2015-03-11 15:28:00 +0800

    open barrier

--- src/com/taobao/hotpatch/OpenBarrier.java
+++ src/com/taobao/hotpatch/OpenBarrier.java
@@ -14,21 +14,21 @@ public class OpenBarrier implements IPatch {
     @Override
     public void handlePatch(PatchParam arg0) throws Throwable {
         Log.e("HotpatchManagerPatch", "handlePath enter");
-        final Context context = arg0.context;
-
-        final Class<?> HotpatchManager = PatchHelper.loadClass(context, "com.taobao.updatecenter.hotpatch.a", null);
-        if (HotpatchManager == null) {
-            Log.e("HotpatchManagerPatch", "class not found, return");
-            return;
-        }
-
-        XposedBridge.findAndHookMethod(HotpatchManager, "b", new XC_MethodReplacement() {
-            @Override
-            protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-                Log.e("HotpatchManagerPatch", "replace enter");
-                return true;
-            }
-        });
+//        final Context context = arg0.context;
+//
+//        final Class<?> HotpatchManager = PatchHelper.loadClass(context, "com.taobao.updatecenter.hotpatch.a", null);
+//        if (HotpatchManager == null) {
+//            Log.e("HotpatchManagerPatch", "class not found, return");
+//            return;
+//        }
+//
+//        XposedBridge.findAndHookMethod(HotpatchManager, "b", new XC_MethodReplacement() {
+//            @Override
+//            protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+//                Log.e("HotpatchManagerPatch", "replace enter");
+//                return true;
+//            }
+//        });
     }
 
 }
