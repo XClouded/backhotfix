2189a1ff9999ffecdcc195f87ce240b2e0febf91
baiyi.hwj <baiyi.hwj@taobao.com>
2015-04-01 19:49:04 +0800

    去 log

--- src/com/taobao/hotpatch/HotpatchBundleInfo.java
+++ src/com/taobao/hotpatch/HotpatchBundleInfo.java
@@ -13,22 +13,20 @@ public class HotpatchBundleInfo implements IPatch{
 
 	@Override
 	public void handlePatch(PatchParam arg0) throws Throwable {
-		Log.e("AtlasInitializerPatch", "handlePatch");
 		Class<?> AtlasInitializerClass = null;
 		try {
 			boolean isMain = PatchHelper.isRunInMainProcess(arg0.context);
 			if (isMain){
 				return;
 			}
-			Log.e("AtlasInitializerPatch", "handlePatch 1");
+
 			AtlasInitializerClass = PatchHelper.loadClass(arg0.context, "com.taobao.tao.atlaswrapper.a",null, this);
-			Log.e("AtlasInitializerPatch", "handlePatch 2");
+
 			Object obj = XposedHelpers.newInstance(AtlasInitializerClass, new Class[] {Application.class,String.class, 
 					Context.class}, RuntimeVariables.androidApplication, "com.taobao.taobao",  arg0.context);
-			Log.e("AtlasInitializerPatch", "handlePatch 3");
+
 			XposedHelpers.callMethod(obj, "d");
-			
-			Log.e("AtlasInitializerPatch", "UpdateBundleInfo invoked!");
+
 		} catch (Throwable e) {
 			return;
 		}
