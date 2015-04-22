3dae5ca268eba88ca619ffab7aadde70eb0356cd
nan.xiangn <nan.xiangn@alibaba-inc.com>
2015-04-22 20:46:09 +0800

    加日志判断是否进来

--- src/com/taobao/hotpatch/agooElectionPatch.java
+++ src/com/taobao/hotpatch/agooElectionPatch.java
@@ -21,6 +21,8 @@ public class agooElectionPatch implements IPatch {
 	// handlePatch这个方法，会在应用进程启动的时候被调用，在这里来实现patch的功能
 	@Override
 	public void handlePatch(PatchParam arg0) throws Throwable {
+		
+		Log.d("agooElectionPatch", "handlePatch,begin.....");
 		// 从arg0里面，可以得到主客的context供使用
 		final Context context = arg0.context;
 		
