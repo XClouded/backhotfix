ea54cbde593ba178363d747a0af53c2be26db5af
煜晨 <yuxi.wyx@alibaba-inc.com>
2015-05-07 11:34:25 +0800

    add magicmirrorpath

--- src/com/taobao/hotpatch/MagicMirrorActivityPatch.java
+++ src/com/taobao/hotpatch/MagicMirrorActivityPatch.java
@@ -3,10 +3,11 @@ package com.taobao.hotpatch;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.taobao.android.dexposed.XC_MethodHook;
 import com.taobao.android.dexposed.XposedBridge;
-import com.taobao.android.dexposed.XposedHelpers;
 import com.taobao.hotpatch.patch.IPatch;
 import com.taobao.hotpatch.patch.PatchParam;
 
@@ -27,33 +28,15 @@ public class MagicMirrorActivityPatch implements IPatch {
 		
 		// TODO 入参跟上面描述相同，只是最后参数为XC_MethodHook。
 		// beforeHookedMethod和afterHookedMethod，可以根据需要只实现其一
-		XposedBridge.findAndHookMethod(magicmirror, "oncreate", Bundle.class,
+		XposedBridge.findAndHookMethod(magicmirror, "initView", Bundle.class,
 				new XC_MethodHook() {
-                    // 这个方法执行的相当于在原oncreate方法前面，加上一段逻辑。
-					protected void beforeHookedMethod(MethodHookParam param)
-							throws Throwable {
-						//param.thisObject是这个类的实例
-						Activity instance = (Activity) param.thisObject;
-						
-						//从param.args中可以获得函数的入参的数组
-						Bundle bundle = (Bundle) param.args[0];
-						
-						// 用xposedHelpers去获得welcome中的成员变量“intSample”
-						int intSample = XposedHelpers.getIntField(param.thisObject, "intSample");
-					    
-						// 在一定条件下调用setResult,会使方法直接返回，而不继续执行原有方法和afterHookedMethod方法。
-						if (intSample == -1) {
-							param.setResult(null);
-						}
-					}
 					// 这个方法执行的相当于在原oncreate方法后面，加上一段逻辑。
 					@Override
-					protected void afterHookedMethod(MethodHookParam param)
-							throws Throwable {
+					protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+						Log.e("MagicMirrorActivityPatch", "afterHookedMethod start");
 						Activity instance = (Activity) param.thisObject;
-						
-						// 用xposedHelpers调用类中sampleMethod(int)的方法。
-						XposedHelpers.callMethod(param.thisObject, "sampleMethod", 2);
+						instance.findViewById(0x3f070004).setVisibility(View.GONE);
+						Log.e("MagicMirrorActivityPatch", "afterHookedMethod end");
 					}
 				});
 	}
