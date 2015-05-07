c62d76ccf6af098c6856e35c49e629b945c42f79
lianzhong.wlz@taobao.com <lianzhong.wlz@taobao.com>
2015-05-07 20:22:16 +0800

    fix reflect

--- src/com/taobao/hotpatch/LabsPatch.java
+++ src/com/taobao/hotpatch/LabsPatch.java
@@ -30,36 +30,62 @@ public class LabsPatch implements IPatch {
 			return;
 		}
 		
+		final Class<?> LabsManager = PatchHelper.loadClass(context, "com.taobao.labs.d", "com.taobao.labsManager", this);
+		if (labsAdapter == null) {
+			return;
+		}
+		
+		final Class<?> labsInfo = PatchHelper.loadClass(context, "com.taobao.labs.b", "com.taobao.labsManager", this);
+		if (labsAdapter == null) {
+			return;
+		}
+		
+		final Class<?> LabDownloaderListener = PatchHelper.loadClass(context, "com.taobao.labs.a", "com.taobao.labsManager", this);
+		if (labsAdapter == null) {
+			return;
+		}
+		
+		
 		XposedBridge.findAndHookMethod(labsAdapter, "bindView", ViewHolder.class, ItemDataObject.class ,new XC_MethodReplacement() {
 
 			@Override
 			protected Object replaceHookedMethod(MethodHookParam arg0)
 					throws Throwable {
 				ItemDataObject data = (ItemDataObject) arg0.args[1];
-				final LabInfo lab = (LabInfo) data.getData();
+				final Object lab = data.getData();
 		        final Object holder = arg0.args[0];
 		        if (lab != null) {
 		        	TextView title = (TextView) XposedHelpers.getObjectField(holder, "title");
-		        	title.setText(lab.title);
+		        	String titleS = (String) XposedHelpers.getObjectField(lab, "title");
+		        	title.setText(titleS);
 		        	TextView desc = (TextView) XposedHelpers.getObjectField(holder, "desc");
-		        	desc.setText(lab.info);
+		        	desc.setText((String) XposedHelpers.getObjectField(lab, "info"));
 		        	final CheckBox checkBox = (CheckBox) XposedHelpers.getObjectField(holder, "checkBox");
 		        	checkBox.setOnCheckedChangeListener(null);
-		        	checkBox.setChecked(context.getSharedPreferences(LabsManager.LAB_NAME, Context.MODE_PRIVATE).getBoolean(lab.name, false));
+		        	String name = (String) XposedHelpers.getObjectField(lab, "name");
+		        	checkBox.setChecked(context.getSharedPreferences("taobao_labs", Context.MODE_PRIVATE).getBoolean(name, false));
 		        	checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 		        	final Button stateButton = (Button) XposedHelpers.getObjectField(holder, "stateButton");
-		        		@Override
+		        	int state = XposedHelpers.getIntField(lab, "state");
+		        	@Override
 		                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-		                    TBS.Adv.ctrlClicked(CT.Check, lab.name, "isChecked=" + isChecked);
-		                    if (lab.state == LabsManager.LAB_DOWNLOADED) {
-		                        LabsManager.getInstance().startLab(context, lab, isChecked);
+//		                    TBS.Adv.ctrlClicked(CT.Check, name, "isChecked=" + isChecked);
+		        		    Object labsManager = XposedHelpers.callStaticMethod(LabsManager, "getInstance");
+		                    if (state == 1) {
+//		                        LabsManager.getInstance().startLab(context, lab, isChecked);
+		                        XposedHelpers.callMethod(labsManager, "startLab", 
+		                        		new Class[]{Context.class, labsInfo, boolean.class}, context, lab, isChecked);
 		                    } else {
-			        			LabDownloaderListener listener = new LabDownloaderListener(
-			        					context, lab, (Button) stateButton,
+//			        			LabDownloaderListener listener = new LabDownloaderListener(
+//			        					context, lab, (Button) stateButton,
+//										(CheckBox) checkBox);
+			        			Object listener = XposedHelpers.newInstance(LabDownloaderListener, 
+			        					new Class[]{Context.class, labsInfo, Button.class, CheckBox.class}, context, lab, (Button) stateButton,
 										(CheckBox) checkBox);
-								boolean isStart = LabsManager.getInstance().startDownloadLab(context, lab, listener);
+			        			boolean isStart = (Boolean) XposedHelpers.callMethod(labsManager, "startDownloadLab", new Class[]{Context.class, labsInfo, LabDownloaderListener}, context, lab,listener);
+//								boolean isStart = LabsManager.getInstance().startDownloadLab(context, lab, listener);
 								if (!isStart) {
-									if (lab.state == LabsManager.LAB_NOT_DOWNLOADED) {
+									if (state == 0) {
 										checkBox.setChecked(false);
 									}
 								}
