a9c791edde9906d2c06036639e9ec34261ef60c9
煜晨 <yuxi.wyx@alibaba-inc.com>
2015-05-07 14:53:04 +0800

    del try'

--- src/com/taobao/hotpatch/MagicMirrorActivityPatch.java
+++ src/com/taobao/hotpatch/MagicMirrorActivityPatch.java
@@ -16,29 +16,25 @@ public class MagicMirrorActivityPatch implements IPatch {
 	// handlePatch这个方法，会在应用进程启动的时候被调用，在这里来实现patch的功能
 	@Override
 	public void handlePatch(PatchParam arg0) throws Throwable {
-		try {
-			// 从arg0里面，可以得到主客的context供使用
-			final Context context = arg0.context;
-			
-			// TODO 这里填上你要patch的bundle中的class名字，第三个参数是所在bundle中manifest的packageName，最后的参数为this
-			Class<?> magicmirror = PatchHelper.loadClass(context, "com.taobao.magicmirror.MagicMirrorActivity", "com.taobao.magicmirror", this);
-			if (magicmirror == null) {
-				return;
-			}
-			
-			// TODO 入参跟上面描述相同，只是最后参数为XC_MethodHook。
-			// beforeHookedMethod和afterHookedMethod，可以根据需要只实现其一
-			XposedBridge.findAndHookMethod(magicmirror, "initView", new XC_MethodHook() {
-						// 这个方法执行的相当于在原initView方法后面，加上一段逻辑。
-						@Override
-						protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-							Activity instance = (Activity) param.thisObject;
-							instance.findViewById(0x3f070004).setVisibility(View.GONE);
-							Log.e("MagicMirrorActivityPatch", "afterHookedMethod");
-						}
-					});
-		} catch (Exception e) {
-			e.printStackTrace();
+		// 从arg0里面，可以得到主客的context供使用
+		final Context context = arg0.context;
+		
+		// TODO 这里填上你要patch的bundle中的class名字，第三个参数是所在bundle中manifest的packageName，最后的参数为this
+		Class<?> magicmirror = PatchHelper.loadClass(context, "com.taobao.magicmirror.MagicMirrorActivity", "com.taobao.magicmirror", this);
+		if (magicmirror == null) {
+			return;
 		}
+		
+		// TODO 入参跟上面描述相同，只是最后参数为XC_MethodHook。
+		// beforeHookedMethod和afterHookedMethod，可以根据需要只实现其一
+		XposedBridge.findAndHookMethod(magicmirror, "initView", new XC_MethodHook() {
+					// 这个方法执行的相当于在原initView方法后面，加上一段逻辑。
+					@Override
+					protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+						Activity instance = (Activity) param.thisObject;
+						instance.findViewById(0x3f070004).setVisibility(View.GONE);
+						Log.e("MagicMirrorActivityPatch", "afterHookedMethod");
+					}
+				});
 	}
 }
