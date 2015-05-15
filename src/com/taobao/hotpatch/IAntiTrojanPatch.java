a9c5775891ddb68f6f9e2c27180fb322c599754f
jojo <jiaojiao.kuangjj@alibaba-inc.com>
2015-05-15 14:25:02 +0800

    增加方法入参

--- src/com/taobao/hotpatch/IAntiTrojanPatch.java
+++ src/com/taobao/hotpatch/IAntiTrojanPatch.java
@@ -20,38 +20,37 @@ import com.taobao.infsword.statistic.KGB;
 import com.taobao.infsword.statistic.KGB.EnvModeEnum;
 
 /**
- *
  * @create May 15, 2015 10:21:32 AM
  * @author jiaojiao.kuangjj
  * @version
  */
-public class IAntiTrojanPatch implements IPatch{
+public class IAntiTrojanPatch implements IPatch {
 
     @Override
     public void handlePatch(PatchParam arg0) throws Throwable {
         // 从arg0里面，可以得到主客的context供使用
         final Context context = arg0.context;
         Log.e("IAntiTrojanPatch", "before find class 1");
-        
+
         // 这里填上你要patch的bundle中的class名字，第三个参数是所在bundle中manifest的packageName，最后的参数为this
         //final Class<?> iAntiTrojan = PatchHelper.loadClass(context, "com.taobao.infsword.client.IAntiTrojan", null, null);
         final Class<?> iAntiTrojan = PatchHelper.loadClass(context, "com.taobao.infsword.client.a", null, null);
         if (iAntiTrojan == null) {
             return;
         }
-        
+
         Log.e("IAntiTrojanPatch", "find class success 1");
-        
+
         // TODO 入参跟上面描述相同，只是最后参数为XC_MethodHook。
         // beforeHookedMethod和afterHookedMethod，可以根据需要只实现其一
-        XposedBridge.findAndHookMethod(iAntiTrojan, "init", new XC_MethodHook() {
-                    // 这个方法执行的相当于在原initView方法后面，加上一段逻辑。
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        KGB.setEnvMode(EnvModeEnum.ONLINE);
-                        Log.e("IAntiTrojanPatch", "beforeHookedMethod 1");
-                    }
-                });
+        XposedBridge.findAndHookMethod(iAntiTrojan, "init", Context.class, String.class, String.class, new XC_MethodHook() {
+            // 这个方法执行的相当于在原initView方法后面，加上一段逻辑。
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                KGB.setEnvMode(EnvModeEnum.ONLINE);
+                Log.e("IAntiTrojanPatch", "beforeHookedMethod 1");
+            }
+        });
     }
 
 }
