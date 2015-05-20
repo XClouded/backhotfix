8adfab0ae145670e62a52727cf688ac670a61329
云魂 <yunhun.fyy@alibaba-inc.com>
2015-05-20 16:19:16 +0800

    add import

--- src/com/taobao/hotpatch/HeadlinePatch.java
+++ src/com/taobao/hotpatch/HeadlinePatch.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 
 import com.taobao.android.dexposed.XC_MethodReplacement;
 import com.taobao.android.dexposed.XposedBridge;
