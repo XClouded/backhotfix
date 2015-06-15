e5cb66585eca24224ec3f2e95d6fed71a31a5edc
武仲 <wuzhong@taobao.com>
2015-06-15 14:20:04 +0800

    update

--- src/com/taobao/hotpatch/ACDSPatcher.java
+++ src/com/taobao/hotpatch/ACDSPatcher.java
@@ -9,6 +9,12 @@ import com.taobao.android.dexposed.XposedBridge;
 import com.taobao.android.dexposed.XposedHelpers;
 import com.taobao.hotpatch.patch.IPatch;
 import com.taobao.hotpatch.patch.PatchParam;
+import com.taobao.tao.Globals;
+import com.taobao.tao.TaoPackageInfo;
+import mtopsdk.mtop.common.MtopListener;
+import mtopsdk.mtop.domain.MtopRequest;
+import mtopsdk.mtop.intf.Mtop;
+import mtopsdk.mtop.intf.MtopBuilder;
 
 // 所有要实现patch某个方法，都需要集成Ipatch这个接口
 public class ACDSPatcher implements IPatch {
@@ -72,8 +78,15 @@ public class ACDSPatcher implements IPatch {
         final Class<?> acdsSwitcher = PatchHelper.loadClass(context, "com.taobao.acds.syncenter.a", "com.taobao.acds", this);
         final Class<?> acdsResponse = PatchHelper.loadClass(context, "com.taobao.acds.protocol.down.ACDSResponse", "com.taobao.acds", this);
 
+        final Class<?> mtopSender = PatchHelper.loadClass(context, "com.taobao.acds.network.g", "com.taobao.acds", this);
+        final Class<?> mtopCallback = PatchHelper.loadClass(context, "com.taobao.acds.network.h", "com.taobao.acds", this);
+        final Class<?> ACCSRequestWrapper = PatchHelper.loadClass(context, "com.taobao.acds.network.ACCSRequestWrapper", "com.taobao.acds", this);
+        final Class<?> AcdsCallback = PatchHelper.loadClass(context, "com.taobao.acds.network.ACDSCallback", "com.taobao.acds", this);
+
+
+
         Log.d("acdspatch", "9");
-        if (null == accsCallback || null == acdsSwitcher) {
+        if (null == accsCallback || null == acdsSwitcher || null == mtopSender || null == mtopCallback || null == ACCSRequestWrapper || null == AcdsCallback) {
             Log.d("acdspatch", "-4");
             return;
         }
@@ -82,7 +95,7 @@ public class ACDSPatcher implements IPatch {
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                 super.beforeHookedMethod(param);
                 Log.d("acdspatch", "4");
-                if(timeoutTimes++ >= 2) {
+                if (timeoutTimes++ >= 2) {
                     accsDeleage = true;
                 }
             }
@@ -98,6 +111,8 @@ public class ACDSPatcher implements IPatch {
             }
         });
 
+
+
         XposedBridge.findAndHookMethod(acdsSwitcher, "isACCSDegrade", String.class, String.class, new XC_MethodReplacement() {
             @Override
             protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
@@ -110,6 +125,37 @@ public class ACDSPatcher implements IPatch {
 
                 return false;
 
+            }
+        });
+
+
+        XposedBridge.findAndHookMethod(mtopSender, "asyncSendData", ACCSRequestWrapper, AcdsCallback, Boolean.class, new XC_MethodReplacement() {
+            @Override
+            protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+
+                Log.d("acdspatch", "11");
+
+                MtopRequest mtopRequest = new MtopRequest();
+                mtopRequest.setApiName("mtop.taobao.sync.service.onReceived");
+                mtopRequest.setVersion("1.0");
+                mtopRequest.setNeedEcode(false);
+                mtopRequest.setNeedSession(true);
+
+                mtopRequest.setData(XposedHelpers.getObjectField(methodHookParam.args[0], "acdsRequest").toString());
+
+                XposedHelpers.callMethod(methodHookParam.args[0],"setDataId","1");
+
+                MtopBuilder mtopBuilder = Mtop.instance(Globals.getApplication()).
+                        build(mtopRequest, TaoPackageInfo.getTTID()).setBizId(42);
+
+                if (null != methodHookParam.args[1] ) {
+                    Log.d("acdspatch", "12");
+                    mtopBuilder.addListener((MtopListener) XposedHelpers.newInstance(mtopCallback));
+                }
+
+                mtopBuilder.asyncRequest();
+
+                return null;
             }
         });
 
