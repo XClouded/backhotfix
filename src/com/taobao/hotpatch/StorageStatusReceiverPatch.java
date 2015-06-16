b6d11ee08778a7908feb46ee4909fec1e40a2734
lianzhong.wlz@taobao.com <lianzhong.wlz@taobao.com>
2015-06-16 14:29:26 +0800

    remove unneeded import

--- src/com/taobao/hotpatch/StorageStatusReceiverPatch.java
+++ src/com/taobao/hotpatch/StorageStatusReceiverPatch.java
@@ -2,14 +2,12 @@ package com.taobao.hotpatch;
 
 import android.content.Context;
 import android.content.Intent;
-import com.taobao.android.dexposed.XC_MethodHook;
+
 import com.taobao.android.dexposed.XC_MethodReplacement;
 import com.taobao.android.dexposed.XposedBridge;
 import com.taobao.android.dexposed.XposedHelpers;
 import com.taobao.hotpatch.patch.IPatch;
 import com.taobao.hotpatch.patch.PatchParam;
-import com.taobao.storagespace.StorageManager;
-import com.taobao.storagespace.StorageStatusReceiver;
 import com.taobao.tao.Globals;
 
 /**
