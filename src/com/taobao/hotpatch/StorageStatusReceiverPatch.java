7fcfeb74144d1a2213c53d53ac3900acd10cbadf
关杰 <guanjie.yjf@taobao.com>
2015-06-17 12:53:54 +0800

    fix

--- src/com/taobao/hotpatch/StorageStatusReceiverPatch.java
+++ src/com/taobao/hotpatch/StorageStatusReceiverPatch.java
@@ -3,6 +3,7 @@ package com.taobao.hotpatch;
 import android.content.Context;
 import android.content.Intent;
 
+import android.util.Log;
 import com.taobao.android.dexposed.XC_MethodReplacement;
 import com.taobao.android.dexposed.XposedBridge;
 import com.taobao.android.dexposed.XposedHelpers;
@@ -17,8 +18,9 @@ public class StorageStatusReceiverPatch  implements IPatch {
     @Override
     public void handlePatch(PatchParam patchParam) throws Throwable {
     	final Context context = patchParam.context;
-    	
-    	final Class<?> StorageStatusReceiver = PatchHelper
+//        Log.e("torageStatusReceiverPatch","patch handlePatch");
+
+        final Class<?> StorageStatusReceiver = PatchHelper
 				.loadClass(
 						context,
 						"com.taobao.storagespace.StorageStatusReceiver",
@@ -30,25 +32,27 @@ public class StorageStatusReceiverPatch  implements IPatch {
     	final Class<?> StorageManager = PatchHelper
 				.loadClass(
 						context,
-						"com.taobao.storagespace.StorageManager",
+						"com.taobao.storagespace.c",
 						null, this);
     	if (StorageManager == null) {
     		return;
     	}
-    	
+//        Log.e("torageStatusReceiverPatch","find class success");
+
         XposedBridge.findAndHookMethod(StorageStatusReceiver,"onReceive",Context.class,
                 Intent.class,new XC_MethodReplacement(){
                     @Override
                     protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
                         Intent intent = (Intent)methodHookParam.args[1];
                         try {
+//                            Log.e("torageStatusReceiverPatch","patch success");
                             if (intent.getAction().equals(Intent.ACTION_DEVICE_STORAGE_LOW)) {
 //                                StorageManager.getInstance(Globals.getApplication()).freeSpace();
 								Object StorageManagerI = XposedHelpers
 										.callStaticMethod(
 												StorageManager,
 												"getInstance",
-												new Class[] { android.app.Application
+												new Class[] { Context
 												.class },
 												Globals.getApplication());
 								XposedHelpers.callMethod(StorageManagerI, "freeSpace");
