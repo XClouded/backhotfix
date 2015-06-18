3f2fdf0c9f0cb30e0fc031ec78514b17f59e9fcd
xianqi.hanxq <xianqi.hanxq@alibaba-inc.com>
2015-06-18 21:37:27 +0800

    add

--- src/com/taobao/hotpatch/DetailPagePatch.java
+++ src/com/taobao/hotpatch/DetailPagePatch.java
@@ -14,56 +14,54 @@ import com.taobao.android.dexposed.XposedBridge;
 import com.taobao.android.dexposed.XposedHelpers;
 import com.taobao.hotpatch.patch.IPatch;
 import com.taobao.hotpatch.patch.PatchParam;
+import com.ut.share.business.StartShareMenuJsBrige;
 
 /**
  * @author shiqing
- *
+ * 
  */
-public class DetailPagePatch  implements IPatch{
-	
-	private static final String TAG = "DetailPagePatch";
-	
+public class DetailPagePatch implements IPatch
+{
+
+	private static final String	TAG	= "DetailPagePatch";
+
 	// handlePatch这个方法，会在应用进程启动的时候被调用，在这里来实现patch的功能
 	@Override
-	public void handlePatch(PatchParam arg0) throws Throwable {
+	public void handlePatch(PatchParam arg0) throws Throwable
+	{
 		// 从arg0里面，可以得到主客的context供使用
 		final Context context = arg0.context;
-		
-		// 这里填上你要patch的bundle中的class名字，第三个参数是所在bundle中manifest的packageName，最后的参数为this
-		final Class<?> startShareMenuJsBrige = PatchHelper.loadClass(context, "com.ut.share.business.StartShareMenuJsBrige", "com.taobao.taobaocompat", this);
-		Log.e(TAG, "startShareMenuJsBrige:"+startShareMenuJsBrige);
-		if (startShareMenuJsBrige == null) {
-			return;
-		}
-	
+
 		final Class<?> detailPage = PatchHelper.loadClass(context, "com.taobao.headline.module.detail.pages.DetailPage", "com.taobao.headline", this);
-		if (detailPage == null) {
+		Log.e(TAG, "detailPage:" + detailPage);
+		if (detailPage == null)
+		{
 			return;
 		}
-		Log.e(TAG, "detailPage:"+detailPage);
-		
+		// Log.e(TAG, "detailPage:"+detailPage);
+
 		// TODO 入参跟上面描述相同，只是最后参数为XC_MethodHook。
 		// beforeHookedMethod和afterHookedMethod，可以根据需要只实现其一
-		XposedBridge.findAndHookMethod(detailPage, "initWebView", Bundle.class, new XC_MethodHook() {
+		XposedBridge.findAndHookMethod(detailPage, "initWebView", Bundle.class, new XC_MethodHook()
+		{
 			@Override
-			protected void afterHookedMethod(MethodHookParam param)
-					throws Throwable {
-				// TODO Auto-generated method stub
+			protected void afterHookedMethod(MethodHookParam param) throws Throwable
+			{
 				super.afterHookedMethod(param);
 			}
-            // 这个方法执行的相当于在原oncreate方法前面，加上一段逻辑。
-			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-				//param.thisObject是这个类的实例
-				WVWebView 	webView=(WVWebView)XposedHelpers.getObjectField(param.thisObject, "mWebView");
-				
-				Activity 	mFrame=(Activity)XposedHelpers.getObjectField(param.thisObject, "frame");
-						
-				Object jsBrige=XposedHelpers.newInstance(startShareMenuJsBrige,new Class[]{mFrame.getClass()},mFrame );
-				Log.e(TAG, mFrame+"y");
-			    webView.addJsObject("TBSharedModule", jsBrige);
-			   
-			    Log.e(TAG, "initWebView");
-				
+
+			// 这个方法执行的相当于在原oncreate方法前面，加上一段逻辑。
+			protected void beforeHookedMethod(MethodHookParam param) throws Throwable
+			{
+				Log.e(TAG, "beforeHookedMethod:" + param);
+				// param.thisObject是这个类的实例
+				WVWebView webView = (WVWebView) XposedHelpers.getObjectField(param.thisObject, "mWebView");
+				Activity mFrame = (Activity) XposedHelpers.getObjectField(param.thisObject, "frame");
+				StartShareMenuJsBrige startShareMenuJsBrige = new StartShareMenuJsBrige(mFrame);
+				webView.addJsObject("TBSharedModule", startShareMenuJsBrige);
+
+				Log.e(TAG, "initWebView");
+
 			}
 		});
 	}
