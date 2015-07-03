d53d3e1705010097047b7109e860391a340e92fe
xieguo.xg <xieguo.xg@alibaba-inc.com>
2015-07-03 03:13:03 -0700

    update.
    Signed-off-by: xieguo.xg <xieguo.xg@alibaba-inc.com>

--- src/com/taobao/hotpatch/HotpatchBundleReplace.java
+++ src/com/taobao/hotpatch/HotpatchBundleReplace.java
@@ -5,16 +5,19 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import org.osgi.framework.BundleException;
 
 import com.taobao.android.dexposed.XC_MethodHook;
 import com.taobao.android.dexposed.XposedBridge;
+import com.taobao.android.dexposed.XposedHelpers;
 import com.taobao.hotpatch.patch.IPatch;
 import com.taobao.hotpatch.patch.PatchParam;
 import com.taobao.statistic.TBS;
 
+import android.content.Context;
 import android.taobao.atlas.framework.Atlas;
 import android.taobao.atlas.framework.BundleImpl;
 import android.util.Log;
@@ -38,26 +41,32 @@ public class HotpatchBundleReplace implements IPatch{
 //				if (android.os.Build.BRAND.equalsIgnoreCase("Meizu") && android.os.Build.VERSION.SDK_INT >= 19){
 			        String soName = "libcom_taobao_android_newtrade.so";
 			        String bundleLocation = "com.taobao.android.newtrade";
-			        ValidateBundle(soName, bundleLocation); 
+			        ValidateBundle(soName, bundleLocation,arg0.context); 
 //				}
 			}
 
 		});
 	}
 
-	private void ValidateBundle(String soName, String bundleLocation)
+	private void ValidateBundle(String soName, String bundleLocation, Context mContext)
 			throws FileNotFoundException, IOException,
-			BundleException {
+			BundleException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		File libDir = new File("/data/data/com.taobao.taobao", "lib");
         File bundleFile = new File(libDir, soName);
         Log.e("HotpatchBundleReplace", "HotpatchBundleReplace 2");
-        BundleImpl b = (BundleImpl) Atlas.getInstance().getBundle(bundleLocation);
+
+        Class<?> AtlasClass = mContext.getClassLoader().loadClass("android.taobao.atlas.framework.a");
+        Object atlasOjb = XposedHelpers.callStaticMethod(AtlasClass, "getInstance");
+        Object bundle = XposedHelpers.callMethod(atlasOjb, "getBundle");
+        
+        
+        Object b = bundle;
         if (b != null){
         	boolean isOldBundle = false;
         	Log.e("HotpatchBundleReplace", "HotpatchBundleReplace 3");
         	try{
 	        	// try to start the bundle
-	        	b.startBundle();
+        		XposedHelpers.callMethod(b, "startBundle");
         	} catch(Exception e){
         		isOldBundle = true;
         	}
@@ -92,15 +101,9 @@ public class HotpatchBundleReplace implements IPatch{
 	            	TBS.Ext.commitEvent(61005, -6, "Old bundle not removed", "", "old rev loc:" + revisionLocation);
 	            	AppBackGroundObserver mAppBackGroundObserver = new AppBackGroundObserver();
 	            	Class<?> globalsClass = Class.forName("com.taobao.tao.Globals");
-	            	Method getApp = globalsClass.getDeclaredMethod("getApplication", null);
-	            	getApp.setAccessible(true);
-	            	Object app = getApp.invoke(null);
+	            	Object app = XposedHelpers.callStaticMethod(globalsClass, "getApplication");
 	            	Log.e("HotpatchBundleReplace", "HotpatchBundleReplace 10");
-	            	Class<?>  panguClass = Class.forName("com.taobao.android.lifecycle.PanguApplication");
-	            	Class<?> paramClass = Class.forName("com.taobao.android.lifecycle.PanguApplication$CrossActivityLifecycleCallback");
-	            	Method rcac = panguClass.getDeclaredMethod("registerCrossActivityLifecycleCallback", paramClass);
-	            	rcac.setAccessible(true);
-	            	rcac.invoke(app, mAppBackGroundObserver);
+	            	XposedHelpers.callMethod(app, "registerCrossActivityLifecycleCallback", mAppBackGroundObserver);
 	            	Log.e("HotpatchBundleReplace", "HotpatchBundleReplace 11");
         		}catch (Exception e){
         			Log.e("HotpatchBundleReplace", "HotpatchBundleReplace 12 " + e);
