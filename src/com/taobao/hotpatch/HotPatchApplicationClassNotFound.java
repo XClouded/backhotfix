f245aa94c1ae1907bdfc46f1f5fade5de7077321
xieguo.xg <xieguo.xg@alibaba-inc.com>
2015-07-08 02:39:06 -0700

    update.
    Signed-off-by: xieguo.xg <xieguo.xg@alibaba-inc.com>

--- src/com/taobao/hotpatch/HotPatchApplicationClassNotFound.java
+++ src/com/taobao/hotpatch/HotPatchApplicationClassNotFound.java
@@ -17,9 +17,7 @@ public class HotPatchApplicationClassNotFound implements IPatch{
 	public void handlePatch(final PatchParam arg0) throws Throwable {
 		Class<?> cls = null;
 		try {
-			Log.e("HotPatchApplicationClassNotFound", "HotPatchApplicationClassNotFound 1");
 			cls = Class.forName("android.taobao.atlas.runtime.BundleLifecycleHandler");
-	        Log.e("HotPatchApplicationClassNotFound", "HotPatchApplicationClassNotFound 2");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
@@ -27,7 +25,6 @@ public class HotPatchApplicationClassNotFound implements IPatch{
 	    XposedBridge.findAndHookMethod(cls, "started", org.osgi.framework.Bundle.class, new XC_MethodReplacement() {
 			@Override
 			protected Object replaceHookedMethod(MethodHookParam arg0) throws Throwable {
-				Log.e("HotPatchApplicationClassNotFound", "HotPatchApplicationClassNotFound");
 	            // load application from AndroidManifest.xml
 				BundleImpl b = (BundleImpl)arg0.args[0];
 				String location = b.getLocation();
@@ -35,24 +32,16 @@ public class HotPatchApplicationClassNotFound implements IPatch{
 				Class<?> clsDelegateComponent = Class.forName("android.taobao.atlas.runtime.e");
             	Class<?> clsBundleLifeCycleHandler = Class.forName("android.taobao.atlas.runtime.BundleLifecycleHandler");
 				Object packageLite = XposedHelpers.callStaticMethod(clsDelegateComponent, "getPackage", location);
-				Log.e("HotPatchApplicationClassNotFound", "HotPatchApplicationClassNotFound 4, bundle is " + b.getLocation());		
 	            if (packageLite != null) {
-					Log.e("HotPatchApplicationClassNotFound", "HotPatchApplicationClassNotFound 5");
 	                String appClassName = (String)XposedHelpers.getObjectField(packageLite, "applicationClassName");
 	                if (appClassName != null && appClassName.length() > 0) {
 	                    try {
-	                    	Log.e("HotPatchApplicationClassNotFound", "HotPatchApplicationClassNotFound 6, bundle is " + b.getLocation());
-	                    	if (true){
-	                    		throw new Throwable("have a test!");
-	                    	}
 	                    	Application app = (Application)XposedHelpers.callStaticMethod(
 	                    			clsBundleLifeCycleHandler, "newApplication", appClassName, bundleClassLoader);
 	                        app.onCreate();
 	                    } catch (Throwable e) {
-	                    	Log.e("HotPatchApplicationClassNotFound", "HotPatchApplicationClassNotFound 7 bundle is" + b.getLocation());
 	                    	if (b.getArchive().isDexOpted() == true){
-	                    		Log.e("HotPatchApplicationClassNotFound", "HotPatchApplicationClassNotFound 8");
-	                    		throw new RuntimeException("atlas-2.3.59", e);   
+	                    		throw new RuntimeException("atlas-2.3.59 dexopt success", e);   
 	                    	} else {
 	                    		try{
 		                    		// not dexopt yet, have another try
@@ -62,12 +51,10 @@ public class HotPatchApplicationClassNotFound implements IPatch{
 	    	                        app.onCreate();
 	                    		}catch (Throwable e1) {
 	                    			if (b.getArchive().isDexOpted() == true){
-	    	                    		Log.e("HotPatchApplicationClassNotFound", "HotPatchApplicationClassNotFound 9");
-	    	                    		throw new RuntimeException("atlas-2.3.59", e1);   
+	    	                    		throw new RuntimeException("atlas-2.3.59 dexopt success", e1);   
 	                    			}
 	                    		}
 	                    	}
-	                    	Log.e("HotPatchApplicationClassNotFound", "HotPatchApplicationClassNotFound 9");
 	                    }
 	                }
 	            }
