afec9ba74e8d9e10476e639818d39a92cf61b94a
shenghua.zhangsh <shenghua.zhangsh@alibaba-inc.com>
2015-07-13 17:49:09 +0800

    解决首页后台摇的问题

---
+++ src/com/taobao/hotpatch/HomepagePatch.java
@@ -0,0 +1,37 @@
+package com.taobao.hotpatch;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.MotionEvent;
+import com.taobao.android.dexposed.XC_MethodReplacement;
+import com.taobao.android.dexposed.XposedBridge;
+import com.taobao.android.dexposed.XposedHelpers;
+import com.taobao.hotpatch.patch.IPatch;
+import com.taobao.hotpatch.patch.PatchParam;
+
+public class HomepagePatch implements IPatch{
+
+	@Override
+	public void handlePatch(PatchParam arg0) throws Throwable {
+
+		final Context context = arg0.context;
+		Log.e("HomepagePatch", "beforeHookedMethod 1");
+		final Class<?> homepageBundleLaunchReceiver = PatchHelper.loadClass(context, "com.taobao.tao.homepage.MainActivity3", "com.taobao.taobao.home", this);
+		if (homepageBundleLaunchReceiver == null){
+			Log.e("HomepagePatch", "class is null");
+			return;
+		}
+		
+		XposedBridge.findAndHookMethod(homepageBundleLaunchReceiver, "dispatchTouchEvent", MotionEvent.class, new XC_MethodReplacement() {
+
+            @Override
+            protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+
+                Log.e("HomepagePatch", "hock method.");
+                XposedHelpers.callStaticMethod(Class.forName("com.taobao.tao.watchdog.a"), "stop");
+
+                return true;
+            }
+		});
+	}
+}
