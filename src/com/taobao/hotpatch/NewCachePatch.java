d4836b637ff121f0c8f97ea9d72f2086a99dd5b8
正凡 <zhaomi.zm@alibaba-inc.com>
2015-08-21 15:51:19 +0800

    comment writeCatalog hook

--- src/com/taobao/hotpatch/NewCachePatch.java
+++ src/com/taobao/hotpatch/NewCachePatch.java
@@ -15,6 +15,7 @@ public class NewCachePatch implements IPatch {
 	public void handlePatch(PatchParam arg0) throws Throwable {
 
 		final Context context = arg0.context;
+		/*
 		final Class<?> MultiNBCacheClass = PatchHelper.loadClass(context, "com.taobao.nbcache.f", null,
 				this);
 		if (MultiNBCacheClass == null) {
@@ -28,7 +29,7 @@ public class NewCachePatch implements IPatch {
 				param.setResult(false);
 			}
 		});
-
+        */
 		final Class<?> PhenixNewCacheClass = PatchHelper.loadClass(context, "com.taobao.phenix.impl.e", null,
 				this);
 		if (PhenixNewCacheClass == null) {
