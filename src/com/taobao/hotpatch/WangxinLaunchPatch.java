bf196dd9e8f5cf04038ca07c5dbf28291e05653c
fangsheng <fangsheng@taobao.com>
2015-08-21 10:10:49 +0800

    修改一种策略

--- src/com/taobao/hotpatch/WangxinLaunchPatch.java
+++ src/com/taobao/hotpatch/WangxinLaunchPatch.java
@@ -1,7 +1,6 @@
 package com.taobao.hotpatch;
 
 import android.content.Context;
-import android.content.Intent;
 import android.util.Log;
 
 import com.taobao.android.dexposed.XC_MethodHook;
@@ -9,29 +8,29 @@ import com.taobao.android.dexposed.XposedBridge;
 import com.taobao.hotpatch.patch.IPatch;
 import com.taobao.hotpatch.patch.PatchParam;
 
-public class WangxinLaunchPatch implements IPatch {
+public class WangxinLaunchPatch implements IPatch{
 
-    @Override
-    public void handlePatch(PatchParam arg0) throws Throwable {
-        final Context context = arg0.context;
-        Log.e("WangxinLaunchPatch", "beforeHookedMethod 1");
-        final Class<?> wxBundleLaunchReceiver = PatchHelper.loadClass(context, "com.taobao.tao.msgcenter.BundleLaunchReceiver", "com.taobao.wangxin", this);
-        if (wxBundleLaunchReceiver == null) {
-            Log.e("WangxinLaunchPatch", "class is null");
-            return;
-        }
-
-        XposedBridge.findAndHookMethod(wxBundleLaunchReceiver, "onReceive", Context.class, Intent.class, new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(MethodHookParam param)
-                    throws Throwable {
-                Intent intent = (Intent) param.args[1];
-                Log.e("WangxinLaunchPatch", intent == null ? "null" : ("intent action=" + intent.getAction()));
-                if (intent != null && "NOTIFY_SESSION_VALID".equals(intent.getAction().toString())) {
-                    param.setResult(null);
-                }
-            }
-        });
-    }
+	@Override
+	public void handlePatch(PatchParam arg0) throws Throwable {
+		final Context context = arg0.context;
+		Log.e("WangxinLaunchPatch", "beforeHookedMethod 1");
+		final Class<?> msgCenterServiceClazz = PatchHelper.loadClass(context, "com.taobao.tao.msgcenter.b", "com.taobao.wangxin", this);
+		if (msgCenterServiceClazz == null){
+			Log.e("WangxinLaunchPatch", "msgCenterServiceClazz is null");
+			return;
+		}
+		
+		XposedBridge.findAndHookMethod(msgCenterServiceClazz, "wxLogin", Boolean.class, new XC_MethodHook() {
+			@Override
+			protected void beforeHookedMethod(MethodHookParam param)
+					throws Throwable {
+				boolean isOnResume = (Boolean)param.args[0];
+				Log.e("WangxinLaunchPatch", "isOnResume=" + isOnResume);
+				if (!isOnResume){
+					param.setResult(null);
+				}
+			}
+		});
+	}
 
 }
