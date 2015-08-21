ce63a62991e86c659514b3490b81932b43ef235a
zhaomi.zm <zhaomi.zm@alibaba-inc.com>
2015-08-21 14:42:15 +0800

    add new-cache patch

---
+++ src/com/taobao/hotpatch/NewCachePatch.java
@@ -0,0 +1,46 @@
+package com.taobao.hotpatch;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.taobao.android.dexposed.XC_MethodHook;
+import com.taobao.android.dexposed.XposedBridge;
+import com.taobao.hotpatch.patch.IPatch;
+import com.taobao.hotpatch.patch.PatchParam;
+
+public class NewCachePatch implements IPatch {
+
+	private static final String TAG = NewCachePatch.class.getSimpleName();
+	@Override
+	public void handlePatch(PatchParam arg0) throws Throwable {
+
+		final Context context = arg0.context;
+		final Class<?> MultiNBCacheClass = PatchHelper.loadClass(context, null, "com.taobao.nbcache.f",
+				this);
+		if (MultiNBCacheClass == null) {
+			return;
+		}
+
+		XposedBridge.findAndHookMethod(MultiNBCacheClass, "writeCatalog", String.class,String.class,int.class,byte[].class,boolean.class,int.class, new XC_MethodHook() {
+			@Override
+			protected void afterHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+				Log.d("NewCachePatch","into writeCatalog afterHookedMethod");
+				param.setResult(false);
+			}
+		});
+
+		final Class<?> PhenixNewCacheClass = PatchHelper.loadClass(context, null, "com.taobao.phenix.impl.e",
+				this);
+		if (PhenixNewCacheClass == null) {
+			return;
+		}
+
+		XposedBridge.findAndHookMethod(PhenixNewCacheClass, "accessNativeCache", new XC_MethodHook() {
+			@Override
+			protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+				Log.d("NewCachePatch","into accessNativeCache beforeHookedMethod");
+				param.setResult(false);
+			}
+		});
+	}
+}
