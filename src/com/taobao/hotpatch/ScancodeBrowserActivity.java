523e030a94f51c7800b294b938fc8b67fe013a01
庐轩 <hansong.lhs@alibaba-inc.com>
2015-08-27 16:00:24 +0800

    scancode patch

---
+++ src/com/taobao/hotpatch/ScancodeBrowserActivity.java
@@ -0,0 +1,51 @@
+package com.taobao.hotpatch;
+
+import android.webkit.WebView;
+import com.taobao.android.dexposed.XC_MethodHook;
+import com.taobao.android.dexposed.XC_MethodReplacement;
+import com.taobao.android.dexposed.XposedBridge;
+import com.taobao.android.dexposed.XposedHelpers;
+import com.taobao.android.dexposed.XC_MethodHook.MethodHookParam;
+import com.taobao.android.nav.Nav;
+import com.taobao.hotpatch.patch.IPatch;
+import com.taobao.hotpatch.patch.PatchParam;
+import android.content.Context;
+import android.util.Log;
+import com.taobao.android.nav.Nav;
+import android.taobao.windvane.webview.WVWebViewClient;
+
+/**
+ * Created by hansonglhs on 15/8/27.
+ */
+public class ScancodeBrowserActivity implements IPatch {
+
+    @Override
+    public void handlePatch(PatchParam arg0) throws Throwable {
+
+        final Context context = arg0.context;
+
+        final Class<?> scancodeBaseBrowserActivityWebViewClient = PatchHelper.loadClass(context, "com.taobao.taobao.scancode.barcode.util.ScancodeBaseBrowserActivity$1", "com.taobao.android.scancode",
+                this);
+        if (scancodeBaseBrowserActivityWebViewClient == null) {
+            return;
+        }
+
+        XposedBridge.findAndHookMethod(scancodeBaseBrowserActivityWebViewClient, "shouldOverrideUrlLoading", WebView.class, String.class, new XC_MethodReplacement() {
+
+                    @Override
+                    protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+                        boolean result = false;
+                        try {
+                            WebView webView = (WebView) methodHookParam.args[0];
+                            String url = (String) methodHookParam.args[1];
+                            Context theContext = webView.getContext().getApplicationContext();
+                            result = Nav.from(theContext).toUri(url);
+                        } catch (Throwable e) {
+
+                        }
+                        return result;
+                    }
+                }
+        );
+    }
+}
