d41d6b06c4c255781870d6d8b26d2ca56122103e
庐轩 <hansong.lhs@alibaba-inc.com>
2015-08-27 17:47:13 +0800

    Scancode Patch, fix inner class invoke bug

--- src/com/taobao/hotpatch/ScancodeBrowserActivity.java
+++ src/com/taobao/hotpatch/ScancodeBrowserActivity.java
@@ -1,5 +1,9 @@
 package com.taobao.hotpatch;
 
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.taobao.windvane.webview.WVWebView;
+import android.view.View;
 import android.webkit.WebView;
 import com.taobao.android.dexposed.XC_MethodHook;
 import com.taobao.android.dexposed.XC_MethodReplacement;
@@ -24,30 +28,52 @@ public class ScancodeBrowserActivity implements IPatch {
 
         final Context context = arg0.context;
 
-        final Class<?> scancodeBaseBrowserActivityWebViewClient = PatchHelper.loadClass(context, "com.taobao.taobao.scancode.barcode.util.ScancodeBaseBrowserActivity$1", "com.taobao.android.scancode",
+        final Class<?> scancodeBaseBrowserActivityWebViewClient = PatchHelper.loadClass(context, "com.taobao.taobao.scancode.barcode.util.ScancodeBaseBrowserActivity", "com.taobao.android.scancode",
                 this);
         if (scancodeBaseBrowserActivityWebViewClient == null) {
-            Log.e("ScancodePatch", "scancodeBaseBrowserActivityWebViewClient is null");
             return;
         }
 
-        Log.e("ScancodePatch", "scancodeBaseBrowserActivityWebViewClient is not null");
-
-        XposedBridge.findAndHookMethod(scancodeBaseBrowserActivityWebViewClient, "shouldOverrideUrlLoading", WebView.class, String.class, new XC_MethodReplacement() {
+        XposedBridge.findAndHookMethod(scancodeBaseBrowserActivityWebViewClient, "init", new XC_MethodHook() {
 
                     @Override
-                    protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
-                        boolean result = false;
-                        Log.e("ScancodePatch", "begin method");
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                         try {
-                            WebView webView = (WebView) methodHookParam.args[0];
-                            String url = (String) methodHookParam.args[1];
-                            Context theContext = webView.getContext().getApplicationContext();
-                            result = Nav.from(theContext).toUri(url);
+                            final WVWebView browserWebView = (WVWebView) XposedHelpers.getObjectField(param.thisObject, "browserWebView");
+                            final View bgView = (View) XposedHelpers.getObjectField(param.thisObject, "bgView");
+                            final View progress = (View) XposedHelpers.getObjectField(param.thisObject, "progress");
+                            final Activity theContext = (Activity) param.thisObject;
+                            browserWebView.setWebViewClient(new WVWebViewClient(context) {
+                                @Override
+                                public void onPageFinished(WebView view, String url) {
+                                    super.onPageFinished(view, url);
+                                    if (bgView.isShown()) {
+                                        bgView.setVisibility(View.GONE);
+                                    }
+                                    if (progress.isShown()) {
+                                        progress.setVisibility(View.INVISIBLE);
+                                    }
+                                }
+
+                                @Override
+                                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                                    if(Nav.from(theContext.getApplicationContext()).toUri(url)) {
+                                        return true;
+                                    }
+                                    return false;
+                                }
+
+                                @Override
+                                public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                                    super.onPageStarted(view, url, favicon);
+                                    if (!progress.isShown()) {
+                                        progress.setVisibility(View.VISIBLE);
+                                    }
+                                }
+                            });
                         } catch (Throwable e) {
                             Log.e("ScancodePatch", e.getLocalizedMessage());
                         }
-                        return result;
                     }
                 }
         );
