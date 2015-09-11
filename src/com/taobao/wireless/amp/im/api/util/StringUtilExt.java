b43bb2ab27d48bb7117a90f8e42a5771f8f91fe6
fangsheng <fangsheng@taobao.com>
2015-09-11 14:21:30 +0800

    新增缺失的类

---
+++ src/com/taobao/wireless/amp/im/api/util/StringUtilExt.java
@@ -0,0 +1,10 @@
+package com.taobao.wireless.amp.im.api.util;
+
+import java.util.List;
+
+public class StringUtilExt {
+
+	public static  <T> String addListToString(String str,List<T> list){
+		return null;
+	}
+}
