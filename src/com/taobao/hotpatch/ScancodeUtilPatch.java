6869e757de806c2c055c44e9e1ee94dd6ba92ee9
庐轩 <hansong.lhs@alibaba-inc.com>
2015-09-15 12:17:50 +0800

    Scancode Util Patch

---
+++ src/com/taobao/hotpatch/ScancodeUtilPatch.java
@@ -0,0 +1,43 @@
+package com.taobao.hotpatch;
+
+import android.content.Context;
+import android.util.Log;
+import com.taobao.android.dexposed.XC_MethodReplacement;
+import com.taobao.android.dexposed.XposedBridge;
+import com.taobao.android.nav.Nav;
+import com.taobao.hotpatch.patch.IPatch;
+import com.taobao.hotpatch.patch.PatchParam;
+
+/**
+ * Created by hansonglhs on 15/9/14.
+ */
+public class ScancodeUtilPatch implements IPatch {
+
+    @Override
+    public void handlePatch(PatchParam arg0) throws Throwable {
+
+        final Context context = arg0.context;
+        final Class<?> scancodeUtil = PatchHelper.loadClass(context, "com.taobao.taobao.scancode.common.b.a", "com.taobao.android.scancode",
+                this);
+        if (scancodeUtil == null) {
+            Log.e("ScancodeUtil", "scancode util is null");
+            return;
+        }
+
+        Log.e("ScancodeUtil", "scancode util is not null");
+
+        XposedBridge.findAndHookMethod(scancodeUtil, "browser", Nav.class, String.class, new XC_MethodReplacement() {
+            @Override
+            protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+                Log.e("ScancodeUtil", "begin nav");
+                try {
+                    Nav nav = (Nav) methodHookParam.args[0];
+                    String targetUrl = (String) methodHookParam.args[1];
+                    return nav.toUri(targetUrl);
+                } catch (Throwable e) {
+                    return false;
+                }
+            }
+        });
+    }
+}
