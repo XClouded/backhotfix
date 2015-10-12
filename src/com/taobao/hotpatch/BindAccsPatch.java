9a2f426f02eda8b44bad82bc510222f669fe700e
nan.xiangn <nan.xiangn@alibaba-inc.com>
2015-10-12 21:21:42 +0800

    判断对象是否存在

--- src/com/taobao/hotpatch/BindAccsPatch.java
+++ src/com/taobao/hotpatch/BindAccsPatch.java
@@ -46,46 +46,47 @@ public class BindAccsPatch implements IPatch{
 		
 		Log.d("BindAccsPatch", "handlePatch agooConfigCrossActivityLifecycleObserver="+agooConfigCrossActivityLifecycleObserver);
 		
-//		// TODO 这里填上你要patch的class名字，根据mapping得到混淆后的名字，在主dex中的class，最后的两个参数均为null
+		// TODO 这里填上你要patch的class名字，根据mapping得到混淆后的名字，在主dex中的class，最后的两个参数均为null
 		final Class<?> agooRegister = PatchHelper.loadClass(context, "com.taobao.tao.pushcenter.a", null,null);
+		Log.d("BindAccsPatch", "handlePatch agooRegister="+agooRegister);
 		if (agooRegister == null) {
 			return;
 		}
 		
 		// TODO 入参跟上面描述相同，只是最后参数为XC_MethodHook。
-				// beforeHookedMethod和afterHookedMethod，可以根据需要只实现其一
-				XposedBridge.findAndHookMethod(agooConfigCrossActivityLifecycleObserver, "onCreated",Activity.class,
-						new XC_MethodHook() {
-							// 这个方法执行的相当于在原oncreate方法后面，加上一段逻辑。
-							@Override
-							protected void afterHookedMethod(MethodHookParam param)
-									throws Throwable {
-								// TODO 把原方法直接考入进这个方法里，然后用反射的方式进行翻译
-								// arg0.thisObject是方法被调用的所在的实例
-								try {
-									Log.d("BindAccsPatch", "XposedBridge.afterHookedMethod begin...");
-									SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
-									boolean flag = settings.getBoolean("is_OpenService", true);
-									SharedPreferences bindSettings = context.getSharedPreferences("bindAccsPatch",
-											Context.MODE_PRIVATE);
-									Editor editor = bindSettings.edit();
-									editor.putBoolean("is_BindAccsPatch", true);
-									editor.commit();
-									Log.d("BindAccsPatch", "startBundle begin...flag="+flag);
-									if(flag){
-										Log.d("BindAccsPatch", "startBundle register...flag="+flag);
-										XposedHelpers.callStaticMethod(agooRegister, "register", new Class[]{Context.class}, context);
-									}else{
-										Log.d("BindAccsPatch", "startBundle register...flag="+flag);
-										XposedHelpers.callStaticMethod(agooRegister, "unRegister", new Class[]{Context.class}, context);
-									}
-								} catch (Throwable e) {
-									Log.e("BindAccsPatch", "BindAccsPatch is error="+e.toString());
-								}
-								
-								
+		// beforeHookedMethod和afterHookedMethod，可以根据需要只实现其一
+		XposedBridge.findAndHookMethod(agooConfigCrossActivityLifecycleObserver, "onCreated",Activity.class,
+				new XC_MethodHook() {
+					// 这个方法执行的相当于在原oncreate方法后面，加上一段逻辑。
+					@Override
+					protected void afterHookedMethod(MethodHookParam param)
+							throws Throwable {
+						// TODO 把原方法直接考入进这个方法里，然后用反射的方式进行翻译
+						// arg0.thisObject是方法被调用的所在的实例
+						try {
+							Log.d("BindAccsPatch", "XposedBridge.afterHookedMethod begin...");
+							SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+							boolean flag = settings.getBoolean("is_OpenService", true);
+							SharedPreferences bindSettings = context.getSharedPreferences("bindAccsPatch",
+									Context.MODE_PRIVATE);
+							Editor editor = bindSettings.edit();
+							editor.putBoolean("is_BindAccsPatch", true);
+							editor.commit();
+							Log.d("BindAccsPatch", "startBundle begin...flag="+flag);
+							if(flag){
+								Log.d("BindAccsPatch", "startBundle register...flag="+flag);
+								XposedHelpers.callStaticMethod(agooRegister, "register", new Class[]{Context.class}, context);
+							}else{
+								Log.d("BindAccsPatch", "startBundle register...flag="+flag);
+								XposedHelpers.callStaticMethod(agooRegister, "unRegister", new Class[]{Context.class}, context);
 							}
-						});
+						} catch (Throwable e) {
+							Log.e("BindAccsPatch", "BindAccsPatch is error="+e.toString());
+						}
+						
+						
+					}
+				});
 		
 	}
 
