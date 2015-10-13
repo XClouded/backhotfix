b23fa8de68724a882931b184c310cdff6cc47238
皙华 <guanhua.zgh@alibaba-inc.com>
2015-10-13 20:17:48 +0800

    动态卡片复用 bug修复

---
+++ src/com/taobao/hotpatch/WxCardRefreshPatch.java
@@ -0,0 +1,43 @@
+package com.taobao.hotpatch;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+
+import com.taobao.android.dexposed.XC_MethodHook;
+import com.taobao.android.dexposed.XposedBridge;
+import com.taobao.android.dexposed.XposedHelpers;
+import com.taobao.hotpatch.patch.IPatch;
+import com.taobao.hotpatch.patch.PatchParam;
+
+public class WxCardRefreshPatch implements IPatch{
+
+	private static String TAG = "WxCardRefreshPatch";
+	
+	@Override
+	public void handlePatch(PatchParam arg0) throws Throwable {
+		final Context context = arg0.context;
+		Log.e(TAG, "patching");
+		final Class<?> weappAdapterClazz = PatchHelper.loadClass(context, "com.taobao.tao.msgcenter.ui.share.WeAppListAdapter", "com.taobao.wangxin", this);
+		final Class<?> weappEnClazz = PatchHelper.loadClass(context, "com.taobao.weapp.tb.b", "com.weapp.taobao.adapter", this);
+		if (weappAdapterClazz == null || weappEnClazz == null){
+			Log.e(TAG, "Clazz is null");
+			return;
+		}
+
+		XposedBridge.findAndHookMethod(weappAdapterClazz, "bindData", new XC_MethodHook() {
+
+			@Override
+			protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+				Log.e(TAG, "afterHookedMethod bindData");
+				Object tag = ((View)param.args[0]).getTag();
+				if(tag == null) return;
+				Class paramTypes[] = new Class[1];
+				paramTypes[0] = weappEnClazz.getClass();
+				XposedHelpers.callMethod(tag,"refresh",paramTypes);
+				Log.e(TAG, "call refresh");
+			}
+		});
+	}
+	
+}
