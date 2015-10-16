912bd92c61cc180b7bf79141741688bd3ba1fc75
瑞克 <anming.xam@alibaba-inc.com>
2015-10-17 00:42:27 +0800

    hook onResume method

--- src/com/taobao/hotpatch/ARMarkerPatch.java
+++ src/com/taobao/hotpatch/ARMarkerPatch.java
@@ -2,16 +2,20 @@ package com.taobao.hotpatch;
 
 import android.app.Activity;
 import android.content.Context;
-import android.os.Build;
+import android.hardware.Camera;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
 import com.taobao.android.dexposed.XC_MethodReplacement;
 import com.taobao.android.dexposed.XposedBridge;
 import com.taobao.android.dexposed.XposedHelpers;
 import com.taobao.hotpatch.patch.IPatch;
 import com.taobao.hotpatch.patch.PatchParam;
-import org.json.JSONObject;
 
 import java.util.List;
 import java.util.Random;
@@ -23,6 +27,10 @@ import java.util.Random;
 public class ARMarkerPatch implements IPatch {
 
     private static final String TAG = ARMarkerPatch.class.getSimpleName();
+
+
+    private static final String OPEN_CAMERA_ERROR = "打开摄像头失败!";
+    private static final String BUNDLE_NAME = "com.taobao.cloakroom";
 
     @Override
     public void handlePatch(PatchParam arg0) throws Throwable {
@@ -38,9 +46,96 @@ public class ARMarkerPatch implements IPatch {
         final Class<?> MarkerAR = PatchHelper.loadClass(context,
                 "com.taobao.t3d.ar.MarkerAR", null, this);
 
-        if (ARMarkerActivity == null || OnPrepareResourceListener == null || ARResource == null || MarkerAR == null) {
+        final Class<?> mARCameraManagerCls = PatchHelper.loadClass(context, "com.taobao.armarker.a.c", BUNDLE_NAME, this);
+
+
+        if (ARMarkerActivity == null || OnPrepareResourceListener == null ||
+                ARResource == null || MarkerAR == null || mARCameraManagerCls == null) {
             return;
         }
+
+
+        XposedBridge.findAndHookMethod(ARMarkerActivity, "onResume", new XC_MethodReplacement() {
+            @Override
+            protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+                try {
+                    Log.i(TAG, "hook updateUTPageName");
+                    Activity instance = (Activity) methodHookParam.thisObject;
+                    Class<?> superClass = instance.getClass().getSuperclass();
+
+                    String mUTPageName = (String) XposedBridge.invokeNonVirtual(instance, superClass.getDeclaredMethod("getUTPageName"));
+                    Log.i(TAG, "pageName = " + mUTPageName);
+                    XposedHelpers.callMethod(instance, "updateUTPageName", mUTPageName);
+
+
+                    // 调用父类中的super.onResume方法。
+                    Log.i(TAG, "Hook super.onResume");
+                    XposedBridge.invokeNonVirtual(instance, superClass.getDeclaredMethod("onResume"));
+
+
+                    SurfaceView surfaceView = (SurfaceView) XposedHelpers.findField(ARMarkerActivity, "mSurfaceView").get(instance);
+                    SurfaceHolder holder = surfaceView.getHolder();
+
+                    Boolean mHasSurface = XposedHelpers.getBooleanField(ARMarkerActivity, "mHasSurface");
+
+                    Object mARCameraManager = null;
+                    if (mHasSurface) {
+                        mARCameraManager = XposedHelpers.callStaticMethod(mARCameraManagerCls, "getInstance");
+                        final Boolean openResult = (Boolean)XposedHelpers.callMethod(mARCameraManager, "a", holder, instance, instance);
+
+                        Log.i(TAG, "openCamera result = " + openResult);
+
+                        if (!openResult) {
+                            Toast.makeText(instance, OPEN_CAMERA_ERROR, Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        // Install the callback and wait for surfaceCreated() to init the camera.
+                        holder.addCallback((SurfaceHolder.Callback)instance);
+                        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+                    }
+
+
+                    Log.i(TAG, "hook T3dGLSurfaceView.onResume");
+                    Object mT3dGLSurfaceView = XposedHelpers.findField(ARMarkerActivity, "mT3dGLSurfaceView").get(instance);
+                    if(null != mT3dGLSurfaceView){
+                        XposedHelpers.callMethod(mT3dGLSurfaceView, "onResume");
+                    }
+
+
+                    final String mCameraParaConfig = (String)XposedHelpers.findField(ARMarkerActivity, "mCameraParaConfig").get(instance);
+                    Log.i(TAG , "mCameraParaConfig = " + mCameraParaConfig);
+
+                    if(!TextUtils.isEmpty(mCameraParaConfig)){
+                        Camera.Size size = (Camera.Size) XposedHelpers.callMethod(mARCameraManager, "b");
+                        if(null != size){
+                            XposedHelpers.callStaticMethod(MarkerAR, "nativeVideoInit", size.width, size.height, 0, false);
+                        }else{
+                            Toast.makeText(instance, OPEN_CAMERA_ERROR, Toast.LENGTH_SHORT).show();
+                            instance.finish();
+                        }
+                    }
+
+
+                    Log.i(TAG, "hook runOnGLThread ");
+                    Class runOnGLThreadParamTypes[] = {Runnable.class};
+                    XposedHelpers.callMethod(mT3dGLSurfaceView, "runOnGLThread",
+                            runOnGLThreadParamTypes, new Runnable() {
+                                @Override
+                                public void run() {
+                                    Class nativeStartParamTypes[] = {String.class};
+                                    XposedHelpers.callStaticMethod(MarkerAR, "nativeStart", nativeStartParamTypes, mCameraParaConfig);
+                                }
+                            });
+
+
+                }catch (Throwable e){
+                    e.printStackTrace();
+                }
+
+                return null;
+            }
+        });
+
 
         XposedBridge.findAndHookMethod(OnPrepareResourceListener, "onPrepareSuccess", ARResource,
                 new XC_MethodReplacement() {
