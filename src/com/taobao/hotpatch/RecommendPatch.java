06effba408778b1c8c7950f6610df10087b2eb0e
照烈 <junjie.fjj@alibaba-inc.com>
2015-10-19 12:51:24 +0800

    推荐sdk npe修复

---
+++ src/com/taobao/hotpatch/RecommendPatch.java
@@ -0,0 +1,48 @@
+package com.taobao.hotpatch;
+
+import android.content.Context;
+import android.util.Log;
+import com.taobao.android.dexposed.XC_MethodHook;
+import com.taobao.android.dexposed.XposedBridge;
+import com.taobao.android.dexposed.XposedHelpers;
+import com.taobao.hotpatch.patch.IPatch;
+import com.taobao.hotpatch.patch.PatchParam;
+
+/**
+ * 解决5.4.3 推荐sdk上一个异常流npe
+ * Created by junjie.fjj on 2015/10/17.
+ */
+public class RecommendPatch implements IPatch {
+
+    private String TAG="RecommendPatch";
+    @Override
+    public void handlePatch(PatchParam patchParam) throws Throwable {
+        final Context context = patchParam.context;
+
+        Class<?> BaseControllerClazz = PatchHelper.loadClass(
+                context, "com.taobao.tao.recommend.a.a", null, this);
+
+        if(BaseControllerClazz==null){
+            Log.e(TAG,"BaseController is null");
+            return;
+        }
+
+        Log.e(TAG,"RecommendPatch invoke");
+
+        XposedBridge.findAndHookMethod(BaseControllerClazz, "a", new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                Object thisObject = param.thisObject;
+                Object viewHolder = XposedHelpers.getObjectField(thisObject,"c");
+                Object viewModel = XposedHelpers.getObjectField(thisObject,"b");
+
+                if(viewHolder==null||viewModel==null){
+                    param.setResult(null);
+                }else{
+                    Log.e(TAG,"viewHolder not null&viewModel not null");
+                }
+            }
+        });
+
+    }
+}
