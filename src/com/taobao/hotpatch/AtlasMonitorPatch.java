7c3dfdc89262e665bbff2d4fcf0106e0ace4fbbd
xieguo.xg <xieguo.xg@alibaba-inc.com>
2015-10-26 14:22:37 +0800

    remove log.
    Signed-off-by: xieguo.xg <xieguo.xg@alibaba-inc.com>

--- src/com/taobao/hotpatch/AtlasMonitorPatch.java
+++ src/com/taobao/hotpatch/AtlasMonitorPatch.java
@@ -10,25 +10,20 @@ import com.taobao.hotpatch.patch.PatchParam;
 
 public class AtlasMonitorPatch implements IPatch {
 
-    private static final String TAG = "AtlasMonitorPatch";
-
     @Override
     public void handlePatch(PatchParam lpparam) throws Throwable {
 
         Context context = lpparam.context;
         Class<?> AtlasMonitor = PatchHelper.loadClass(context, "android.taobao.atlas.util.e", null, this); //android.taobao.atlas.util.AtlasMonitor 
 
-        Log.e(TAG, " tag 1");
         XposedBridge.findAndHookMethod(AtlasMonitor, "trace", Integer.class, String.class,String.class,String.class,new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
             	try{
 					Integer traceId = (Integer) param.args[0];
 					String bundleName =  (String) param.args[1];
-	                Log.e(TAG, " tag 2, traceId = " + traceId + " bundleName = " + bundleName);
 	                
 					if ((traceId == 7) || bundleName.equals("com.taobao.barrier")){
-						Log.e(TAG, " tag 3");
 						param.setResult(null);
 					}
 	            }catch (Exception e){
